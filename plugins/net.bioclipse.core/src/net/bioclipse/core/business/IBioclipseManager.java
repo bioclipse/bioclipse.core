4c0f64dff6d9200368378e09fe24defe3f74112a
ospjuth <ospjuth@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2008-03-05 13:29:45 +0000

    Added base interface for managers
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@5010 fcb5ba71-d80d-0410-8237-ba3920747fcc

---
+++ plugins/net.bioclipse.core/src/net/bioclipse/core/business/IBioclipseManager.java
@@ -0,0 +1,11 @@
+package net.bioclipse.core.business;
+
+public interface IBioclipseManager {
+
+	/**
+	 * This is the namespace that will be used in scripting
+	 * @return
+	 */
+	public String getNamespace();
+	
+}
