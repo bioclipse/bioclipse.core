a05a24dfca55f482271d10d6e2f808a39569bb78
ospjuth <ospjuth@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2008-03-10 18:13:25 +0000

    Added a convenience class to cache images.
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@5075 fcb5ba71-d80d-0410-8237-ba3920747fcc

---
+++ plugins/net.bioclipse.core/src/net/bioclipse/core/util/ImageCache.java
@@ -0,0 +1,52 @@
+/*******************************************************************************
+ * Copyright (c) 2008 The Bioclipse Project and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Ola Spjuth
+ *     
+ ******************************************************************************/
+package net.bioclipse.core.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.swt.graphics.Image;
+
+/**
+ * Cache images to save memory and time
+ * 
+ * @author ola
+ *
+ */
+public class ImageCache {
+	private final Map<ImageDescriptor, Image> imageMap;
+
+	public ImageCache() {
+		super();
+		imageMap = new HashMap<ImageDescriptor, Image>();	
+	}
+
+	@SuppressWarnings("unchecked")
+	public Image getImage(ImageDescriptor imageDescriptor) {
+		if (imageDescriptor == null)
+			return null;
+		Image image = (Image) imageMap.get(imageDescriptor);
+		if (image == null) {
+			image = imageDescriptor.createImage();
+			imageMap.put(imageDescriptor, image);
+		}
+		return image;
+	}
+
+	public void dispose() {
+		for (Image img : imageMap.values()){
+			img.dispose();
+		}
+		imageMap.clear();
+	}
+}
