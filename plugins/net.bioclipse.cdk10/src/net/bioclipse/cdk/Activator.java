9e7a99ac8316ebbbfe48a607ddd76563dbd0bc13
egonw <egonw@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2008-03-17 16:14:15 +0000

    Added some missing stuff
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@5110 fcb5ba71-d80d-0410-8237-ba3920747fcc

---
+++ plugins/net.bioclipse.cdk10/src/net/bioclipse/cdk/Activator.java
@@ -0,0 +1,11 @@
+package net.bioclipse.cdk;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+
+public class Activator extends AbstractUIPlugin {
+
+	public Activator() {
+		// really, nothing much to do.
+	}
+
+}
