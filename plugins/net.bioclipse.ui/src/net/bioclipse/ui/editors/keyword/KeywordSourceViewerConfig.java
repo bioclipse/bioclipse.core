5a2a3f7e25e2d7f89b2925fdea6610a7e6be0f98
carl_masak <carl_masak@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2008-03-19 13:26:52 +0000

    [KeywordSourceViewerConfig.java] [FileImportWizard.java] * removed unused imports
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@5133 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.ui/src/net/bioclipse/ui/editors/keyword/KeywordSourceViewerConfig.java
+++ plugins/net.bioclipse.ui/src/net/bioclipse/ui/editors/keyword/KeywordSourceViewerConfig.java
@@ -12,19 +12,14 @@
 package net.bioclipse.ui.editors.keyword;
 
 import org.eclipse.jface.text.IDocument;
-import org.eclipse.jface.text.TextAttribute;
 import org.eclipse.jface.text.contentassist.ContentAssistant;
 import org.eclipse.jface.text.contentassist.IContentAssistant;
 import org.eclipse.jface.text.presentation.IPresentationReconciler;
 import org.eclipse.jface.text.presentation.PresentationReconciler;
 import org.eclipse.jface.text.rules.DefaultDamagerRepairer;
 import org.eclipse.jface.text.rules.RuleBasedScanner;
-import org.eclipse.jface.text.rules.Token;
 import org.eclipse.jface.text.source.ISourceViewer;
 import org.eclipse.jface.text.source.SourceViewerConfiguration;
-import org.eclipse.swt.graphics.Color;
-import org.eclipse.swt.graphics.RGB;
-import org.eclipse.swt.widgets.Display;
  
 /**
  * Configuration for the JmolEditor.
--- plugins/net.bioclipse.ui/src/net/bioclipse/ui/importWizards/FileImportWizard.java
+++ plugins/net.bioclipse.ui/src/net/bioclipse/ui/importWizards/FileImportWizard.java
@@ -15,7 +15,6 @@ import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.ui.IImportWizard;
 import org.eclipse.ui.IWorkbench;
-import org.eclipse.ui.wizards.datatransfer.FileSystemImportWizard;
 
 public class FileImportWizard extends Wizard implements IImportWizard {
 	
