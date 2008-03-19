2dd1e2743f3cabe63baa9cf882a58711115353d9
rklancer <rklancer@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2008-03-19 21:16:34 +0000

    Updated to reflect new fragment project method of logging configuration. Used better method for Spring extender startup.
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@5145 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.ui/src/net/bioclipse/ui/Activator.java
+++ plugins/net.bioclipse.ui/src/net/bioclipse/ui/Activator.java
@@ -13,13 +13,14 @@
 package net.bioclipse.ui;
 
 import java.net.URL;
+import java.io.File;
 
 import net.bioclipse.recording.IHistory;
 
-import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
-import org.apache.log4j.Level;
+import org.apache.log4j.FileAppender;
 import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.core.runtime.Platform;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 import org.osgi.util.tracker.ServiceTracker;
@@ -35,13 +36,17 @@ public class Activator extends BioclipseActivator {
     // The plug-in ID
     public static final String PLUGIN_ID = "net.bioclipse.ui";
 
+    // TODO remove - we actually use log4j.properties in a net.bioclipse.log4jconfig
     // The file for logger properties
     private static final String LOG_PROPERTIES_FILE = "logger.properties";
 
     public final ConsoleEchoer CONSOLE = new ConsoleEchoer();
 
     private ServiceTracker finderTracker;
-
+    
+    private static Logger logger = Logger.getLogger(Activator.class);
+    private static String FILE_APPENDER_NAME = "file";
+    
     // The shared instance.
     private static Activator plugin;
 
@@ -76,36 +81,29 @@ public class Activator extends BioclipseActivator {
         return plugin;
     }
 
+    
     @Override
     public void start(BundleContext context) throws Exception {
 
-        // configure logging
-
-        // TODO
-        // the right way to configure logging is to keep lazy loading of log4j
-        // by listening for its bundle start event (and of course to use file
-        // based configuration)
-
-        BasicConfigurator.configure();
-        Logger.getRootLogger().setLevel(Level.DEBUG);
-        Logger.getLogger("org.springframework").setLevel(Level.WARN);
-
         super.start(context);
 
-        // make sure to start Spring Bundle Extender before any extendees
+        String actualLogFileName = getActualLogFileName();
+        if (actualLogFileName == null)
+            System.err.println("WARNING: Bioclipse log file may not be configured.");       
+        else
+            logger.info("Log file location: " + actualLogFileName);
 
-        for (Bundle b : context.getBundles()) {
-            if ("org.springframework.osgi.bundle.extender".equals(
-                    b.getSymbolicName())) {
+        // Make sure to start Spring Bundle Extender before any extendees
+        // there should be only one, right? Better to fail if there are two...
+        
+        Bundle b = Platform.getBundle("org.springframework.osgi.bundle.extender");
+       
+        // turns out using the no-arg start() would mark the bundle to be
+        // autostarted at next startup--this would cause a race wrt log4j config
 
-                // turns out using the no-arg start() marks the bundle to be
-                // autostarted at next startup--causes a race wrt log4j config
-
-                b.start(Bundle.START_TRANSIENT);
-                break;
-            }
-        }
-
+        b.start(Bundle.START_TRANSIENT);
+        logger.debug("Spring Bundle Extender started.");       
+        
         finderTracker = new ServiceTracker(context, IHistory.class.getName(),
                 null);
         finderTracker.open();
@@ -124,4 +122,22 @@ public class Activator extends BioclipseActivator {
         }
         return history;
     }
+
+    private String getActualLogFileName() {
+
+        FileAppender appender = ((FileAppender) Logger.getRootLogger()
+                .getAppender(FILE_APPENDER_NAME));
+        if (appender == null)
+            return null;
+        
+        String requestedLogFileName = appender.getFile();
+        if (requestedLogFileName == null)
+            return null;
+        
+        String actualLogFileName = (new File(requestedLogFileName))
+                .getAbsolutePath();
+        
+        return actualLogFileName;
+    }
 }
+
