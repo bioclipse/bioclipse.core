843ed45355b4f308f81f33ffcf97039389475d4c
ospjuth <ospjuth@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2008-03-28 09:33:20 +0000

    Removed @overrides as the Bioclipse project currently supports Java 1.5.
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@5248 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.usermanager/src/net/bioclipse/usermanager/business/UserManager.java
+++ plugins/net.bioclipse.usermanager/src/net/bioclipse/usermanager/business/UserManager.java
@@ -26,114 +26,92 @@ public class UserManager implements IUserManager {
 		listeners = new ArrayList<IUserManagerListener>();
 	}
 	
-	@Override
 	public boolean accountExists(String accountId) {
 		return userContainer.accountExists(accountId);
 	}
 
-	@Override
 	public void changePassword(String oldkey, String newkey) {
 		userContainer.changePassword(oldkey, newkey);
 	}
 
-	@Override
 	public void clearAccounts() {
 		userContainer.clearAccounts();
 	}
 
-	@Override
 	public void createAccount( String accountId,
 			                   HashMap<String, String> properties, 
 			                   AccountType accountType ) {
 		userContainer.createAccount(accountId, properties, accountType);
 	}
 
-	@Override
 	public void createUser(String userName, String key) {
 		userContainer.createUser(userName, key);
 	}
 
-	@Override
 	public void deleteUser(String user) {
 		userContainer.deleteUser(user);
 	}
 
-	@Override
 	public List<String> getAccountIdsByAccountTypeName(String accountTypeName) {
 		return userContainer.getAccountIdsByAccountTypeName(accountTypeName);
 	}
 
-	@Override
 	public AccountType getAccountType(String accountId) {
 		return userContainer.getAccountType(accountId);
 	}
 
-	@Override
 	public String[] getAvailableAccountTypeNames() {
 		return userContainer.getAvailableAccountTypeNames();
 	}
 
-	@Override
 	public AccountType[] getAvailableAccountTypes() {
 		return userContainer.getAvailableAccountTypes();
 	}
 
-	@Override
 	public User getLoggedInUser() {
 		return userContainer.getLoggedInUser();
 	}
 
-	@Override
 	public String getLoggedInUserName() {
 		return userContainer.getLoggedInUserName();
 	}
 
-	@Override
 	public Collection<String> getLoggedInUsersAccountNames() {
 		return userContainer.getLoggedInUsersAccountNames();
 	}
 
-	@Override
 	public String getProperty(String accountId, String propertyKey) {
 		return userContainer.getProperty(accountId, propertyKey);
 	}
 
-	@Override
 	public Collection<String> getPropertyKeys(String accountId) {
 		return userContainer.getPropertyKeys(accountId);
 	}
 
-	@Override
 	public List<String> getUserNames() {
 		return userContainer.getUserNames();
 	}
 
-	@Override
 	public boolean isLoggedIn() {
 		return userContainer.isLoggedIn();
 	}
 
-	@Override
 	public boolean isLoggedInWithAccountType(String accountTypeName) {
 		return userContainer.isLoggedInWithAccountType(accountTypeName);
 	}
 
-	@Override
 	public void persist() {
 		userContainer.persist();
 	}
 
-	@Override
 	public void reloadFromFile() {
 		userContainer.reloadFromFile();
 	}
 
-	@Override
 	public void signIn(String username, String password) {
 		userContainer.signIn(username, password, null);
 	}
 
-	@Override
 	public void signInWithProgressBar( String username, 
 			                           String password,
 			                           SubProgressMonitor monitor ) {
@@ -146,23 +124,19 @@ public class UserManager implements IUserManager {
 		fireLoginWithProgressBar( subMonitor );
 	}
 
-	@Override
 	public void signOut() {
 		userContainer.signOut();
 		fireLogout();
 	}
 
-	@Override
 	public String getNamespace() {
 		return "userManager";
 	}
 
-	@Override
 	public UserContainer getSandBoxUserContainer() {
 		return userContainer.clone();
 	}
 
-	@Override
 	public void switchUserContainer(UserContainer userContainer) {
 		this.userContainer = userContainer;
 		fireUpdate();
