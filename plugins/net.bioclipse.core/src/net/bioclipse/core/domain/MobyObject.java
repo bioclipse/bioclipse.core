4dd8d6ea60fe638aab223bd64d00e48e466c9f32
ospjuth <ospjuth@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2008-03-31 13:49:08 +0000

    Added new IMobyObject and MobyObject impl for BioObjects that has a namespace and ID.
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@5294 fcb5ba71-d80d-0410-8237-ba3920747fcc

---
+++ plugins/net.bioclipse.core/src/net/bioclipse/core/domain/MobyObject.java
@@ -0,0 +1,39 @@
+/*******************************************************************************
+ * Copyright (c) 2008 The Bioclipse Project and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Ola Spjuth
+ *     
+ ******************************************************************************/
+package net.bioclipse.core.domain;
+
+public class MobyObject extends BioObject{
+
+	private String id;
+	private String namespace;
+	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getNamespace() {
+		return namespace;
+	}
+
+	public void setNamespace(String namespace) {
+		this.namespace = namespace;
+	}
+
+	public Object getAdapter(Class adapter) {
+		return null;
+	}
+
+}
