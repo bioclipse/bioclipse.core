e0df4f35f7bef018ae2402240d3066d91bdfbfd3
jonalv <jonalv@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2008-04-07 15:45:58 +0000

    removed testBug1934611 since it had absolutely nothing to to with the bug and turned out to test things that are more or less trivially true
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@5376 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.core.tests/net.bioclipse.recording/net/bioclipse/core/domain/BioListTest.java
+++ plugins/net.bioclipse.core.tests/net.bioclipse.recording/net/bioclipse/core/domain/BioListTest.java
@@ -184,32 +184,4 @@ public class BioListTest {
 	public void testSubList() {
 		fail("this method not implemented in BioList. It is complicated...");
 	}
-	
-	@Test
-	public void testBug1934611() {
-		BiojavaSequenceList blist = new BiojavaSequenceList();
-		for ( BiojavaSequence seq 
-			 : new BiojavaSequence[] { new BiojavaSequence(),
-				                       new BiojavaSequence() } ) {
-			
-			if (seq != null){
-				System.out.println("Blist contains before addition: ");
-				for ( ISequence sew:blist ) {
-					System.out.println("  " + sew.getName());
-				}
-				blist.add(seq);
-				System.out.println("Added sequence: " 
-						           + seq.getName() + " to returnlist");
-				
-				System.out.println("Blist contains after addition: ");
-				for (ISequence sew:blist){
-					System.out.println("  " + sew.getName());
-				}
-			} 
-			else {
-				System.out.println("Could not add sequence to returnlist");
-			}
-		}
-		assertEquals( 2, blist.size() );
-	}
 }
