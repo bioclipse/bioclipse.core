f80a27b0264a4b249e389fc71933c2d44e1f3335
egonw <egonw@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2008-04-25 08:06:31 +0000

    Template for an editor; time for the 'Eclipse Building Commercial-Quality Plug-ins' book
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@5483 fcb5ba71-d80d-0410-8237-ba3920747fcc

---
+++ plugins/net.bioclipse.cdk10.jchempaint/src/net/bioclipse/cdk10/jchempaint/Activator.java
@@ -0,0 +1,11 @@
+package net.bioclipse.cdk10.jchempaint;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+
+public class Activator extends AbstractUIPlugin {
+
+	public Activator() {
+		// really, nothing much to do.
+	}
+
+}
---
+++ plugins/net.bioclipse.cdk10.jchempaint/src/net/bioclipse/cdk10/jchempaint/ui/editor/MDLMolfileEditor.java
@@ -0,0 +1,18 @@
+package net.bioclipse.cdk10.jchempaint.ui.editor;
+
+import net.bioclipse.ui.Activator;
+import net.bioclipse.ui.editors.keyword.KeywordEditor;
+
+import org.apache.log4j.Logger;
+
+public class MDLMolfileEditor extends KeywordEditor {
+	
+	private static final Logger logger =
+		Activator.getLogManager().getLogger(MDLMolfileEditor.class.toString());
+
+	public MDLMolfileEditor() {
+		super();
+	}
+
+	
+}
