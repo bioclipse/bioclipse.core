4f56ed2340b8c263d5c179d7fd32348c4b60d72b
egonw <egonw@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2008-06-01 11:35:21 +0000

    Trying to make sense why BC2 no longer boots for me: don't eat throwables...
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@5766 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.ui/src/net/bioclipse/ui/Application.java
+++ plugins/net.bioclipse.ui/src/net/bioclipse/ui/Application.java
@@ -3,7 +3,7 @@
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
- * www.eclipse.orgÑepl-v10.html <http://www.eclipse.org/legal/epl-v10.html>
+ * www.eclipse.orgï¿½epl-v10.html <http://www.eclipse.org/legal/epl-v10.html>
  * 
  * Contributors:
  *     Ola Spjuth - initial API and implementation
@@ -33,9 +33,13 @@ public class Application implements IApplication {
                 return IApplication.EXIT_RESTART;
             }
             return IApplication.EXIT_OK;
+        } catch (Throwable exception) {
+            System.out.println("Error while booting Bioclipse: " + exception.getMessage());
+            exception.printStackTrace();
         } finally {
             display.dispose();
         }
+        return IApplication.EXIT_OK;
     }
 
     /* (non-Javadoc)
