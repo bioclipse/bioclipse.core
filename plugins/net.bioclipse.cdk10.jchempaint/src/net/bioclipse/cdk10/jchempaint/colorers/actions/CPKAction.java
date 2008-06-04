1ad61db7deeb735a2bc6e0d5c11102960f7faa30
ospjuth <ospjuth@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2008-06-04 12:31:36 +0000

    Added ColorByCPK action.
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@5803 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.cdk10.jchempaint/src/net/bioclipse/cdk10/jchempaint/colorers/actions/CPKAction.java
+++ plugins/net.bioclipse.cdk10.jchempaint/src/net/bioclipse/cdk10/jchempaint/colorers/actions/CPKAction.java
@@ -1,7 +1,19 @@
 package net.bioclipse.cdk10.jchempaint.colorers.actions;
 
+import java.awt.Font;
+import java.util.HashMap;
+
+import net.bioclipse.cdk10.business.ICDK10Constants;
+import net.bioclipse.cdk10.jchempaint.ui.editor.IJCPBasedEditor;
+
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.IAction;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.PlatformUI;
+import org.openscience.cdk.interfaces.IAtom;
+import org.openscience.cdk.interfaces.IAtomContainer;
+import org.openscience.cdk.interfaces.IChemModel;
+import org.openscience.cdk.renderer.Renderer2DModel;
 
 
 public class CPKAction extends Action implements IAction{
@@ -9,7 +21,47 @@ public class CPKAction extends Action implements IAction{
     @Override
     public void run() {
 
-        System.out.println("WEE");
+        IEditorPart part = PlatformUI.getWorkbench().getActiveWorkbenchWindow()
+        .getActivePage().getActiveEditor();
+        
+
+        if (!( part instanceof IJCPBasedEditor )) {
+            return;
+        }
+
+        IJCPBasedEditor editor = (IJCPBasedEditor) part;
+        IChemModel chemModel= editor.getChemModel();
+        if (chemModel==null) return;
+        if (chemModel.getMoleculeSet()==null) return;
+
+        
+        //Get AtomContainer from model
+        IAtomContainer ac = chemModel.getMoleculeSet().getMolecule( 0 );
+        
+        //Do GUI customizations
+        Renderer2DModel model=editor.getDrawingPanel().getRenderer2D()
+        .getRenderer2DModel();            
+
+        //Color by CPK, so set props for color to NULL
+        model.getColorHash().clear();
+        model.getToolTipTextMap().clear();
+
+        for (int i=0; i< ac.getAtomCount(); i++){
+            IAtom atom=ac.getAtom( i );
+            atom.removeProperty( ICDK10Constants.COLOR_PROPERTY);
+        }
+        
+        //Configure JCP
+        model.setKekuleStructure( false );
+        model.setShowAtomTypeNames( false );
+        model.setShowImplicitHydrogens( false );
+        model.setShowExplicitHydrogens(  false );
+        Font font = new Font("courier", Font.PLAIN, 14);
+        model.setFont(font);
+        
+        //Update drawing
+        model.fireChange();
+
         
     }
     
