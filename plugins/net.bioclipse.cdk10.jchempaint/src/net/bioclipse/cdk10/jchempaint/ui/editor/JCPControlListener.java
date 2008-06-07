89c366861c235105d390d9dcc6f2aa28a5738aa5
miguelrojasch <miguelrojasch@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2008-06-07 12:00:43 +0000

    JCP rendering reactions
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@5818 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.cdk10.jchempaint/src/net/bioclipse/cdk10/jchempaint/ui/editor/JCPControlListener.java
+++ plugins/net.bioclipse.cdk10.jchempaint/src/net/bioclipse/cdk10/jchempaint/ui/editor/JCPControlListener.java
@@ -1,6 +1,8 @@
 package net.bioclipse.cdk10.jchempaint.ui.editor;
 
 import java.awt.Dimension;
+import java.util.Iterator;
+import java.util.List;
 
 import javax.vecmath.Point2d;
 
@@ -13,7 +15,9 @@ import org.openscience.cdk.geometry.GeometryTools;
 import org.openscience.cdk.interfaces.IAtom;
 import org.openscience.cdk.interfaces.IAtomContainer;
 import org.openscience.cdk.interfaces.IChemModel;
+import org.openscience.cdk.interfaces.IReaction;
 import org.openscience.cdk.renderer.Renderer2DModel;
+import org.openscience.cdk.tools.manipulator.ReactionManipulator;
 
 /**
  * Control Listener for JCP_SWT 
@@ -62,9 +66,23 @@ public class JCPControlListener implements ControlListener {
      */
     private void rendererPositions(){
         IChemModel editorModel = jcpEditor.getJcpModel().getChemModel();
-        Renderer2DModel r2dm = jcpEditor.getJcpModel().getRendererModel();
         
-        IAtomContainer molecule = editorModel.getMoleculeSet().getAtomContainer(0);
+        if(editorModel.getMoleculeSet() != null && 
+        		editorModel.getMoleculeSet().getAtomContainer(0) != null)
+        	renderPosition_IAtomContainer(editorModel.getMoleculeSet().getAtomContainer(0));
+        else if(editorModel.getReactionSet() != null){
+        	IReaction reaction = editorModel.getReactionSet().getReaction(0);
+        	renderPosition_IReaction(reaction);
+        }
+    }
+    
+    /**
+     * Rendering the position of the IAtomContainer within of the Composite.
+     * 
+     * @param molecule The IAtomContainer to renderer
+     */
+    private void renderPosition_IAtomContainer(IAtomContainer molecule){
+        Renderer2DModel r2dm = jcpEditor.getJcpModel().getRendererModel();
 
         double scaleFactor = GeometryTools.getScaleFactor(molecule ,r2dm.getBondLength(),r2dm.getRenderingCoordinates());
         GeometryTools.scaleMolecule(molecule, scaleFactor,r2dm.getRenderingCoordinates());
@@ -114,13 +132,72 @@ public class JCPControlListener implements ControlListener {
             hScroll.setThumb(Math.min(size.y+space, rect.width-space));
             resultW=size.y+space;
         }
-        //System.err.println(resultW+"  "+resultH);
+//        System.err.println(resultW+"  "+resultH);
         r2dm.setBackgroundDimension(new Dimension(resultW,resultH));
         GeometryTools.center(molecule, r2dm.getBackgroundDimension(),r2dm.getRenderingCoordinates());
-        //for(int i=0;i<molecule.getAtomCount();i++){
-        //    System.err.println(i+" "+molecule.getAtomAt(i).getPoint2d()+"  "+((Point2d)r2dm.getRenderingCoordinate(molecule.getAtomAt(i))));
-        //}
+//        for(int i=0;i<molecule.getAtomCount();i++){
+//            System.err.println(i+" "+molecule.getAtom(i).getPoint2d()+"  "+((Point2d)r2dm.getRenderingCoordinate(molecule.getAtom(i))));
+//        }
     
+    }
+    /**
+     * Rendering the position of the IReaction within of the Composite.
+     * 
+     * @param reaction The IReaction to renderer
+     */
+    private void renderPosition_IReaction(IReaction reaction){
+    	Renderer2DModel r2dm = jcpEditor.getJcpModel().getRendererModel();
+    	List<IAtomContainer> acList = ReactionManipulator.getAllAtomContainers(reaction);
+    	for(Iterator<IAtomContainer> it = acList.iterator(); it.hasNext();){
+    		IAtomContainer molecule = it.next();
+    		double scaleFactor = GeometryTools.getScaleFactor(molecule ,r2dm.getBondLength(),r2dm.getRenderingCoordinates());
+            GeometryTools.scaleMolecule(molecule, scaleFactor,r2dm.getRenderingCoordinates());
+    	}
+    	/* height*/
+        double[] result = getRectangle(reaction,r2dm);
+        int height = jcpEditor.getJcpComposite().getClientArea().height;
+        int reactionHeight = ((int)result[1]-(int)result[3]);
+
+        /* width */
+        int width = jcpEditor.getJcpComposite().getClientArea().width;
+        int reactionWidth = ((int)result[0]-(int)result[2]);
+        
+        Point size = new Point(reactionHeight,reactionWidth);
+        Rectangle rect = jcpEditor.getJcpComposite().getClientArea();
+        ScrollBar vScroll = jcpEditor.getJcpScrollBar().vertical;
+        ScrollBar hScroll = jcpEditor.getJcpScrollBar().horizontal;
+        int space = 20;
+        int resultPlus = space;
+        int resultH = (int)result[1]+resultPlus;
+        if(height>(reactionHeight+space)){
+            resultPlus = (height-(reactionHeight))/2;
+            vScroll.setVisible(false);
+            resultH=height;
+        }else {
+            vScroll.setVisible(true);
+            vScroll.setPageIncrement((size.x+space)/10);
+            vScroll.setMaximum(size.x+space);
+            vScroll.setThumb(Math.min(size.x+space, rect.height-space));
+            resultH=size.x+space;
+        }
+
+        resultPlus = space;
+        int resultW = (int)result[0]+resultPlus;
+        if(width>(reactionWidth+space)){
+            resultPlus = (width-(reactionWidth))/2;
+            hScroll.setVisible(false);
+            resultW=width;
+        }else {
+            hScroll.setVisible(true);
+            hScroll.setMaximum(size.y+space);
+            vScroll.setPageIncrement((size.y+space)/10);
+            hScroll.setThumb(Math.min(size.y+space, rect.width-space));
+            resultW=size.y+space;
+        }
+        r2dm.setBackgroundDimension(new Dimension(resultW,resultH));
+    	GeometryTools.center(reaction, r2dm.getBackgroundDimension(),r2dm.getRenderingCoordinates());
+            
+    	
     }
     /**
      * get the rectangle position of a molecule
@@ -144,6 +221,36 @@ public class JCPControlListener implements ControlListener {
             if(minY < ((Point2d)r2dm.getRenderingCoordinate(atom)).y)
                 minY = ((Point2d)r2dm.getRenderingCoordinate(atom)).y;
         
+        }
+        double[] result ={minX,minY,maxX,maxY}; 
+        return result;
+    }
+    /**
+     * get the rectangle position of a reaction
+     * 
+     * @param reaction  the reaction to extract the position
+     * @return          coordenates of the rectangle
+     */
+    public double[] getRectangle(IReaction reaction, Renderer2DModel r2dm){
+        
+        double maxX = Double.MAX_VALUE,maxY=Double.MAX_VALUE,minX=Double.MIN_VALUE,minY=Double.MIN_VALUE;
+        List<IAtomContainer> acList = ReactionManipulator.getAllAtomContainers(reaction);
+        for(Iterator<IAtomContainer> it = acList.iterator(); it.hasNext();){
+	    	IAtomContainer mol = it.next();
+        	for(int j = 0 ; j < mol.getAtomCount(); j++){
+	            IAtom atom = mol.getAtom(j);    
+	                
+	            if(maxX > ((Point2d)r2dm.getRenderingCoordinate(atom)).x)
+	                maxX = ((Point2d)r2dm.getRenderingCoordinate(atom)).x;
+	            if(maxY > ((Point2d)r2dm.getRenderingCoordinate(atom)).y)
+	                maxY = ((Point2d)r2dm.getRenderingCoordinate(atom)).y;
+	            
+	            if(minX < ((Point2d)r2dm.getRenderingCoordinate(atom)).x)
+	                minX = ((Point2d)r2dm.getRenderingCoordinate(atom)).x;
+	            if(minY < ((Point2d)r2dm.getRenderingCoordinate(atom)).y)
+	                minY = ((Point2d)r2dm.getRenderingCoordinate(atom)).y;
+	        
+	        }
         }
         double[] result ={minX,minY,maxX,maxY}; 
         return result;
