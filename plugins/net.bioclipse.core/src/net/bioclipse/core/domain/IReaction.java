10964c3da3b28733d602d61b0814e3da7666cfe3
miguelrojasch <miguelrojasch@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2008-06-09 08:17:00 +0000

    Added reaction domain
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@5824 fcb5ba71-d80d-0410-8237-ba3920747fcc

---
+++ plugins/net.bioclipse.core/src/net/bioclipse/core/domain/IReaction.java
@@ -0,0 +1,39 @@
+/*******************************************************************************
+ * Copyright (c) 2008 The Bioclipse Project and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * www.eclipse.org—epl-v10.html <http://www.eclipse.org/legal/epl-v10.html>
+ * 
+ * Contributors:
+ *     Ola Spjuth
+ *     
+ ******************************************************************************/
+package net.bioclipse.core.domain;
+
+import net.bioclipse.core.business.BioclipseException;
+
+/**
+ * An interface to represent a Reaction that can be represented with 
+ * Smiles notation. Since we can get SMILES, we can also get 2D coordinates.
+ * Interface also requires the IReaction to be able to be serialized as CML.
+ * 
+ * @author miguelrojasch
+ *
+ */
+public interface IReaction extends IBioObject{
+
+    /**
+     * 
+     * @return the SMILES string for this IReaction
+     * @throws BioclipseException
+     */
+    public String getSmiles() throws BioclipseException;
+
+    /**
+     * @return the IReaction serialized to CML
+     * @throws BioclipseException if serialization fails
+     */
+    public String getCML() throws BioclipseException;
+    
+}
