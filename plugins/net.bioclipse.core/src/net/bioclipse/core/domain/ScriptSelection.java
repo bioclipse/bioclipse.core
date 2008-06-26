66e29161fb80537265275e84f5b2fb720ddf9d59
ospjuth <ospjuth@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2008-06-26 08:55:02 +0000

    Added namespace to ScriptSelection.
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@5946 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.core/src/net/bioclipse/core/domain/ScriptSelection.java
+++ plugins/net.bioclipse.core/src/net/bioclipse/core/domain/ScriptSelection.java
@@ -1,17 +1,48 @@
 package net.bioclipse.core.domain;
 
-
+/**
+ * Provides means to send scripts, where target is identified by a namspace
+ * Example of namespace is "jmol"
+ * 
+ * @author ola
+ *
+ */
 public class ScriptSelection implements IChemicalSelection{
 
     private String script;
+    private String namespace;
     
-    public ScriptSelection(String script) {
+    public ScriptSelection(String namespace, String script) {
         super();
         this.script = script;
     }
 
     public Object getSelection() {
         return script;
+    }
+
+    
+    public String getScript() {
+    
+        return script;
+    }
+
+    
+    public void setScript( String script ) {
+    
+        this.script = script;
+    }
+
+    
+    public String getNamespace() {
+    
+        return namespace;
+    }
+
+    
+    public void setNamespace( String namespace ) {
+    
+        this.namespace = namespace;
     }
     
     
