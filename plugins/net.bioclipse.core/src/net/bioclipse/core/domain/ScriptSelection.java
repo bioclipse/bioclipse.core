5e366951ec4238c085ed74b37ee0bc62b1997395
ospjuth <ospjuth@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2008-06-26 09:08:47 +0000

    Added constructores and documented code.
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@5948 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.core/src/net/bioclipse/core/domain/ScriptSelection.java
+++ plugins/net.bioclipse.core/src/net/bioclipse/core/domain/ScriptSelection.java
@@ -1,7 +1,6 @@
 package net.bioclipse.core.domain;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 /**
@@ -15,9 +14,22 @@ public class ScriptSelection implements IChemicalSelection{
 
     //Each command is stored in a map, with Script -> Namespace
     private Map<String, String> scripts;
-    
+
+    /**
+     * Create an empty ScriptSelection
+     */
     public ScriptSelection() {
         scripts=new HashMap<String, String>();
+    }
+
+    /**
+     * Create a new ScriptSelection and add a first script
+     * @param script the script to provide
+     * @param namespace a namespace for the intended target
+     */
+    public ScriptSelection(String script, String namespace) {
+        this();
+        addScript( script, namespace );
     }
 
     public Object getSelection() {
