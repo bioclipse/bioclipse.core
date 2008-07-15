570ce1ebacd387f70a81635192246e03867e88ae
carl_masak <carl_masak@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2008-07-15 11:30:29 +0000

    [JCPMultiPageEditorContributor] * imports * whitespace, indentation, long lines * rewrote setupPopupMenus to use data structure  instead of control structures
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@6160 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.cdk10.jchempaint/src/net/bioclipse/cdk10/jchempaint/ui/editor/JCPMultiPageEditorContributor.java
+++ plugins/net.bioclipse.cdk10.jchempaint/src/net/bioclipse/cdk10/jchempaint/ui/editor/JCPMultiPageEditorContributor.java
@@ -5,9 +5,9 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.StringTokenizer;
 
-import net.bioclipse.cdk10.jchempaint.Activator;
 import net.bioclipse.cdk10.jchempaint.ui.editor.action.JCPAction;
 
 import org.eclipse.core.commands.operations.IUndoContext;
@@ -35,22 +35,25 @@ import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.openscience.cdk.Atom;
 import org.openscience.cdk.Bond;
 import org.openscience.cdk.ChemModel;
+import org.openscience.cdk.DefaultChemObjectBuilder;
 import org.openscience.cdk.PseudoAtom;
 import org.openscience.cdk.Reaction;
 import org.openscience.cdk.applications.jchempaint.JChemPaintModel;
 import org.openscience.cdk.controller.PopupController2D;
 import org.openscience.cdk.event.ICDKChangeListener;
 import org.openscience.cdk.interfaces.IChemModel;
+import org.openscience.cdk.interfaces.IMolecule;
 import org.openscience.cdk.renderer.Renderer2DModel;
 import org.openscience.cdk.smiles.SmilesParser;
 
 /**
- * Manages the installation/deinstallation of global actions for multi-page editors.
- * Responsible for the redirection of global actions to the active editor.
- * Multi-page contributor replaces the contributors for the individual editors in the multi-page editor.
+ * Manages the installation/uninstallation of global actions for multi-page
+ * editors. Responsible for the redirection of global actions to the active
+ * editor. Multi-page contributor replaces the contributors for the individual
+ * editors in the multi-page editor.
  */
 public class JCPMultiPageEditorContributor extends MultiPageEditorActionBarContributor {
-    private ArrayList actionList;
+    private ArrayList<?> actionList;
     private IEditorPart activeEditorPart;
     public JCPAction lastaction= null;
     
@@ -72,13 +75,17 @@ public class JCPMultiPageEditorContributor extends MultiPageEditorActionBarContr
         actionList = ToolBarMaker.createToolbar(this);
 //        sampleAction = new Action() {
 //            public void run() {
-//                MessageDialog.openInformation(null, "Bc_jcp_swt Plug-in", "Sample Action Executed");
+//                MessageDialog.openInformation(
+//                    null,
+//                    "Bc_jcp_swt Plug-in",
+//                    "Sample Action Executed");
 //            }
 //        };
 //        sampleAction.setText("Sample Action");
 //        sampleAction.setToolTipText("Sample Action tool tip");
-//        sampleAction.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages().
-//                getImageDescriptor(IDE.SharedImages.IMG_OBJS_TASK_TSK));
+//        sampleAction.setImageDescriptor(PlatformUI.getWorkbench()
+//                    .getSharedImages()
+//                    .getImageDescriptor(IDE.SharedImages.IMG_OBJS_TASK_TSK));
     }
     public void contributeToMenu(IMenuManager manager) {
         IMenuManager menu = new MenuManager("JChemPaint");
@@ -121,12 +128,14 @@ public class JCPMultiPageEditorContributor extends MultiPageEditorActionBarContr
         IExtensionPoint serviceObjectExtensionPoint = registry
         .getExtensionPoint("net.bioclipse.cdk10.colorer");
         
-        if (serviceObjectExtensionPoint==null) return null;
+        if (serviceObjectExtensionPoint==null)
+            return null;
 
         IExtension[] serviceObjectExtensions 
         = serviceObjectExtensionPoint.getExtensions();
 
-        if (serviceObjectExtensions==null || serviceObjectExtensions.length<=0) return null;
+        if (serviceObjectExtensions==null || serviceObjectExtensions.length<=0)
+            return null;
 
         for(IExtension extension : serviceObjectExtensions) {
             for( IConfigurationElement element 
@@ -134,22 +143,29 @@ public class JCPMultiPageEditorContributor extends MultiPageEditorActionBarContr
 
                 if (element.getName().equals("colorer")){
                     try {
-                        IAction action=(IAction) element.createExecutableExtension( "class" );
+                        IAction action
+                            = (IAction)element
+                              .createExecutableExtension("class");
                         action.setText( element.getAttribute("name") );
                         action.setId( element.getAttribute("id") );
                         String iconpath=element.getAttribute("icon");
                         
                         if (iconpath!=null){
-                            String ns = element.getDeclaringExtension().getNamespaceIdentifier();
-                            ImageDescriptor desc =
-                              AbstractUIPlugin.imageDescriptorFromPlugin(ns, iconpath); 
+                            String ns
+                                = element.getDeclaringExtension()
+                                         .getNamespaceIdentifier();
+                            ImageDescriptor desc
+                              = AbstractUIPlugin.imageDescriptorFromPlugin(
+                                  ns,
+                                  iconpath); 
                             
                             action.setImageDescriptor( desc );
                         }
                         colorers.add( action );
                         System.out.println("Added action: " + action.getText());
                     } catch ( CoreException e ) {
-                        System.out.println("Could not add action: " + element.getAttribute("name"));
+                        System.out.println("Could not add action: "
+                                           + element.getAttribute("name"));
                     }
                 }
                 }
@@ -227,8 +243,9 @@ public class JCPMultiPageEditorContributor extends MultiPageEditorActionBarContr
             this.activeEditorPart = part;
 //            super.setActiveEditor(part);
             if (((IJCPBasedEditor)activeEditorPart).getJcpModel() != null) {
-                // this is done in setActivePage() as well don't want to register more than once
-//                registerModel(((IJCPBasedEditor)activeEditorPart).getJcpModel());
+                // this is done in setActivePage() as well don't want to
+                // register more than once
+//              registerModel(((IJCPBasedEditor)activeEditorPart).getJcpModel());
                 // FIXME egonw: likely crucial call
 //                ((IJCPBasedEditor)activeEditorPart).setContributor(this);
             }
@@ -249,144 +266,187 @@ public class JCPMultiPageEditorContributor extends MultiPageEditorActionBarContr
             IUndoContext undoContext=null;
             JCPPage jcpPage = null;
             PopupController2D inputAdapter = null;
+
             if ( activeEditorPart instanceof IJCPBasedEditor ) {
-                drawingPanel = ((IJCPBasedEditor)activeEditorPart).getDrawingPanel();
+                drawingPanel
+                  = ((IJCPBasedEditor)activeEditorPart).getDrawingPanel();
                 jcpcomp=((IJCPBasedEditor)activeEditorPart).getJcpComposite();
-                undoContext=((IJCPBasedEditor)this.getActiveEditorPart()).getUndoContext();
-                jcpPage=((IJCPBasedEditor)this.getActiveEditorPart()).getJCPPage();
-                }
+                undoContext
+                  = ((IJCPBasedEditor)this.getActiveEditorPart())
+                    .getUndoContext();
+                jcpPage
+                  = ((IJCPBasedEditor)this.getActiveEditorPart()).getJCPPage();
+            }
             else if ( activeEditorPart instanceof JCPPage ) {
                 drawingPanel = ((JCPPage)activeEditorPart).getDrawingPanel();
-                jcpcomp=(JCPComposite) ((JCPPage)activeEditorPart).getJcpComposite();
-                //undoContext=(((JCPPage)this.getActiveEditorPart()).getMPE()).getUndoContext();
+                jcpcomp = (JCPComposite)
+                          ((JCPPage)activeEditorPart).getJcpComposite();
+                // undoContext = 
+                //     (((JCPPage)this.getActiveEditorPart()).getMPE())
+                //     .getUndoContext();
                 jcpPage = (JCPPage)this.getActiveEditorPart();
             }
-            else return;
+            else {
+                return;
+            }
             
             /*new code -  functional group*/
-            String filename = "org/openscience/cdk/applications/jchempaint/resources/text/funcgroups.txt";
-            InputStream ins = this.getClass().getClassLoader().getResourceAsStream(filename);
+            String filename
+              = "org/openscience/cdk/applications/jchempaint/"
+                + "resources/text/funcgroups.txt";
+            InputStream ins
+              = this.getClass().getClassLoader().getResourceAsStream(filename);
             
-            HashMap funcgroups = new HashMap();
-            SmilesParser sp=new SmilesParser();
-            StringBuffer sb=new StringBuffer();
+            HashMap<String, IMolecule> funcgroups
+                = new HashMap<String, IMolecule>();
+            SmilesParser sp
+                = new SmilesParser(DefaultChemObjectBuilder.getInstance());
+            StringBuffer sb = new StringBuffer();
             if (ins != null) {
                 InputStreamReader isr = new InputStreamReader(ins);
-                try{
-                    while(true){
-                        int i=isr.read();
-                        if(i==-1){
+                try {
+                    while(true) {
+                        int i = isr.read();
+                        if( i == -1 ){
                             break;
-                        }else if(((char)i)=='\n' || ((char)i)=='\r'){
-                            if(!sb.toString().equals("")){
-                                StringTokenizer st=new StringTokenizer(sb.toString());
-                                String key=(String)st.nextElement();
-                                String value=(String)st.nextElement();
-                                funcgroups.put(key, sp.parseSmiles(value));
-                                funcgroups.put(key.toUpperCase(), sp.parseSmiles(value));
-                                sb=new StringBuffer();
+                        }
+                        else if ( ((char)i)=='\n' || ((char)i)=='\r' ) {
+                            if ( sb.length() > 0 ) {
+                                StringTokenizer st
+                                  = new StringTokenizer(sb.toString());
+                                String key = (String)st.nextElement();
+                                IMolecule value = sp.parseSmiles(
+                                    (String)st.nextElement()
+                                );
+                                funcgroups.put(key, value);
+                                funcgroups.put(key.toUpperCase(), value);
+                                sb = new StringBuffer();
                             }
-                        }else{
+                        }
+                        else {
                             sb.append((char)i);
                         }
                     }
-                    if(!sb.toString().equals("")){
-                        StringTokenizer st=new StringTokenizer(sb.toString());
-                        String key=(String)st.nextElement();
-                        String value=(String)st.nextElement();
-                        funcgroups.put(key, sp.parseSmiles(value));
-                        funcgroups.put(key.toUpperCase(), sp.parseSmiles(value));
+                    if( sb.length() > 0 ) {
+                        StringTokenizer st = new StringTokenizer(sb.toString());
+                        String key = (String)st.nextElement();
+                        IMolecule value = sp.parseSmiles(
+                            (String)st.nextElement()
+                        );
+                        funcgroups.put(key, value);
+                        funcgroups.put(key.toUpperCase(), value);
                     }
-                }catch(Exception ex){
+                }
+                catch(Exception ex) {
                     ex.printStackTrace();
                 }
             }
+
             inputAdapter=jcpPage.getInputAdapter();
-            if(inputAdapter==null){
-            inputAdapter = new BCJCPPopupController(
-                (ChemModel) model.getChemModel(), 
-                model.getRendererModel(),model.getControllerModel(), null, null, 
-                jcpcomp,funcgroups);
+            if(inputAdapter==null) {
+                inputAdapter = new BCJCPPopupController(
+                    (ChemModel) model.getChemModel(), 
+
+                    model.getRendererModel(),
+                    model.getControllerModel(),
+                    null,
+                    null,
+
+                    jcpcomp, funcgroups);
+
                 jcpPage.setInputAdapter(inputAdapter);
                 
                 inputAdapter.addCDKChangeListener(model);
-                //drawingPanel.setJChemPaintModel(model);
+                // drawingPanel.setJChemPaintModel(model);
                 drawingPanel.addMouseListener(inputAdapter);
                 drawingPanel.addMouseMotionListener(inputAdapter);
-                //Somehow this registration does not work. If it would, element symbols could be changed via keyboard
+                // Somehow this registration does not work. If it did,
+                // element symbols could be changed via keyboard
                 drawingPanel.addKeyListener(inputAdapter);
                 if(activeEditorPart instanceof JCPPage)
-                inputAdapter.addCDKChangeListener(((JCPPage)activeEditorPart));
+                    inputAdapter.addCDKChangeListener(
+                        ((JCPPage)activeEditorPart)
+                    );
             }
             
-                
-            
             // Undo/Redo stuff
-            JCPBioclipseUndoRedoHandler undoRedoHandler=new JCPBioclipseUndoRedoHandler();
+            JCPBioclipseUndoRedoHandler undoRedoHandler
+                = new JCPBioclipseUndoRedoHandler();
             undoRedoHandler.setDrawingPanel(drawingPanel);
             undoRedoHandler.setJcpm(model);
             undoRedoHandler.setUndoContext(undoContext);
             inputAdapter.setUndoRedoHandler(undoRedoHandler);
             
-            
             setupPopupMenus(inputAdapter);
             Renderer2DModel rendererModel = model.getRendererModel();
-            model.getControllerModel().setBondPointerLength(rendererModel.getBondLength());
-            model.getControllerModel().setRingPointerLength(rendererModel.getBondLength());
+            model.getControllerModel().setBondPointerLength(
+              rendererModel.getBondLength()
+            );
+            model.getControllerModel().setRingPointerLength(
+              rendererModel.getBondLength()
+            );
             
             if ( activeEditorPart instanceof ICDKChangeListener ) {
-                ICDKChangeListener cdkPart = (ICDKChangeListener) activeEditorPart;
-                model.getRendererModel().addCDKChangeListener(cdkPart);
+              ICDKChangeListener cdkPart
+                = (ICDKChangeListener) activeEditorPart;
+              model.getRendererModel().addCDKChangeListener(cdkPart);
             }
             else if (activeEditorPart instanceof IJCPBasedEditor) {
-                IJCPBasedEditor jcpEdPart = (IJCPBasedEditor) activeEditorPart;
-                model.getRendererModel().addCDKChangeListener(jcpEdPart.getJCPPage());
+              IJCPBasedEditor jcpEdPart = (IJCPBasedEditor) activeEditorPart;
+              model.getRendererModel().addCDKChangeListener(
+                jcpEdPart.getJCPPage()
+              );
             }
             else if (activeEditorPart instanceof JCPPage) {
-                model.getRendererModel().addCDKChangeListener((JCPPage)activeEditorPart);
+              model.getRendererModel().addCDKChangeListener(
+                (JCPPage)activeEditorPart
+                );
             }
-            
         }
     }
     
     public void updateModel(IChemModel chemModel){
         
-        DrawingPanel drawingPanel =null;
+        DrawingPanel drawingPanel = null;
         if ( activeEditorPart instanceof IJCPBasedEditor ) {
-            drawingPanel = ((IJCPBasedEditor)activeEditorPart).getDrawingPanel();
+            drawingPanel
+              = ((IJCPBasedEditor)activeEditorPart).getDrawingPanel();
         }
         else if ( activeEditorPart instanceof JCPPage ) {
-            drawingPanel = ((JCPPage)activeEditorPart).getDrawingPanel();
+            drawingPanel
+              = ((JCPPage)activeEditorPart).getDrawingPanel();
             
         }
-        else return;
+        else {
+            return;
+        }
 
         
-        ((PopupController2D)drawingPanel.getKeyListeners()[0]).setChemModel(chemModel);
+        ((PopupController2D)drawingPanel.getKeyListeners()[0])
+            .setChemModel(chemModel);
+        
         drawingPanel.updateRingSetInRenderer();
     }
     
+    @SuppressWarnings("serial")
     public void setupPopupMenus(PopupController2D inputAdapter)
     {
-        if (inputAdapter.getPopupMenu(Atom.class) == null)
-        {
-            inputAdapter.setPopupMenu(Atom.class, new JChemPaintPopupMenu(this, "atom"));
-        }
-        if (inputAdapter.getPopupMenu(PseudoAtom.class) == null)
-        {
-            inputAdapter.setPopupMenu(PseudoAtom.class, new JChemPaintPopupMenu(this, "pseudo"));
-        }
-        if (inputAdapter.getPopupMenu(Bond.class) == null)
-        {
-            inputAdapter.setPopupMenu(Bond.class, new JChemPaintPopupMenu(this, "bond"));
-        }
-        if (inputAdapter.getPopupMenu(ChemModel.class) == null)
-        {
-            inputAdapter.setPopupMenu(ChemModel.class, new JChemPaintPopupMenu(this, "chemmodel"));
-        }
-        if (inputAdapter.getPopupMenu(Reaction.class) == null)
-        {
-            inputAdapter.setPopupMenu(Reaction.class, new JChemPaintPopupMenu(this, "reaction"));
+        Map<Class<?>, String> menuItems
+            = new HashMap<Class<?>, String>() {{
+                put(Atom.class,       "atom");
+                put(PseudoAtom.class, "pseudo");
+                put(Bond.class,       "bond");
+                put(ChemModel.class,  "chemmodel");
+                put(Reaction.class,   "reaction");
+              }};
+        
+        for (Map.Entry<Class<?>, String> e : menuItems.entrySet()) {
+            if (inputAdapter.getPopupMenu(e.getKey()) == null) {
+                inputAdapter.setPopupMenu(
+                    e.getKey(),
+                    new JChemPaintPopupMenu(this, e.getValue())
+                );
+            }
         }
     }
 }
