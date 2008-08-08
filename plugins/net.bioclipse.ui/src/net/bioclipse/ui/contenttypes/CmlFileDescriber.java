4f067191caa95e60893ee9bccffa340d9bc644b6
gilleain <gilleain@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2008-08-08 12:22:21 +0000

    Changed the CmlFileDescriber to use the XmlPullParser (xpp), also added the jar as a library dependency.
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@6539 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.ui/src/net/bioclipse/ui/contenttypes/CmlFileDescriber.java
+++ plugins/net.bioclipse.ui/src/net/bioclipse/ui/contenttypes/CmlFileDescriber.java
@@ -1,6 +1,5 @@
 package net.bioclipse.ui.contenttypes;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -16,17 +15,19 @@ import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.content.IContentDescription;
 import org.eclipse.osgi.util.NLS;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
 
 @SuppressWarnings("restriction")
 public class CmlFileDescriber extends TextContentDescriber 
 							implements IExecutableExtension {
-
+	
 	private Hashtable elements = null;
 
 	/**
 	 * Store parameters
 	 */
-	@SuppressWarnings("unchecked")
 	public void setInitializationData(final IConfigurationElement config, final String propertyName, final Object data) throws CoreException {
 		if (data instanceof String) {
 			// why would this happen?
@@ -39,71 +40,97 @@ public class CmlFileDescriber extends TextContentDescriber
 			throw new CoreException(new Status(IStatus.ERROR, ContentMessages.OWNER_NAME, 0, message, null));
 		}
 	}
-
+	
 	/**
 	 * Determine what the CML file contains by quickly scanning the InputStream.
 	 */
 	public int describe(InputStream contents, IContentDescription description) throws IOException {
-		return analyse(new BufferedReader(new InputStreamReader(contents)));
+		return analyse(new InputStreamReader(contents), description);
 	}
 	
 	/**
 	 * Determine what the CML file contains by quickly scanning the Reader.
 	 */
 	public int describe(Reader contents, IContentDescription description) throws IOException {
-		return analyse(new BufferedReader(contents));
+		return analyse(contents, description);
 	}
 
 	/**
-	 * Read 100 chars, search for x2 or x2
+	 * Scan the document, looking for certain key features.
 	 */
-	private int analyse(BufferedReader input) throws IOException {
-
+	private int analyse(Reader input, IContentDescription description) throws IOException {
+		/*
+		 * Check the CML for molecule tags, and check to see if it is 2D or 3D.
+		 */
 		boolean has2D = false;
 		boolean has3D = false;
+		boolean searching = true;
 		int moleculeCount = 0;
-		
+
+		try {
+			XmlPullParserFactory factory = 
+				XmlPullParserFactory.newInstance(
+						System.getProperty(XmlPullParserFactory.PROPERTY_NAME), null);
+			factory.setNamespaceAware(false);
+			factory.setValidating(false);
+
+			XmlPullParser parser = factory.newPullParser();
+			parser.setInput(input);
+			while (parser.next() != XmlPullParser.END_DOCUMENT) {
+				if (parser.getEventType() == XmlPullParser.START_TAG) {
+					if ("molecule".equalsIgnoreCase(parser.getName())) {
+						moleculeCount++;
+					}
+
+					/*
+					 * Search for the first example of an 'x2' or 'x3'
+					 * attribute to be found in an 'atom' tag. This means
+					 * that a mixed 2D/3D file will not be seen as such. 
+					 */
+					search:
+						if (searching && "atom".equalsIgnoreCase(parser.getName())) {
+							int attributeCount = parser.getAttributeCount();
+							for (int i = 0; i < attributeCount; i++) {
+								String attributeName = parser.getAttributeName(i);
+								if (attributeName.equalsIgnoreCase("x2")) {
+									has2D = true;
+									searching = false;
+									break search;
+								}
+								if (attributeName.equalsIgnoreCase("x3")) {
+									has3D = true;
+									searching = false;
+									break search;
+								}
+							}
+						}
+				}
+			}
+		} catch (XmlPullParserException xppe) {
+			xppe.printStackTrace(System.err);
+		}
+
+
 		/*
-		 * Sadly, we have to scan the entire file.
-		 * This is because we won't know if there are multiple
-		 * molecules until all the "/molecule"s are seen.
-		 * It might be that a full, SAX style solution would
-		 * be better for this.
+		 * Compare what was found with what the Describer expects. 
 		 */
-		String line;
-		while ((line = input.readLine()) != null) {
-			if (line.contains("x2")) {
-				has2D = true;
-			}
-			if (line.contains("x3")) {
-				has3D = true;
-			}
-			if (line.contains("/molecule")) {
-				moleculeCount++;
-			}
-		}
-		
-		// there might be a way to handle mixed 2D/3D at some point
-		if (has2D && has3D) {
-			return INDETERMINATE;
-		}
 
 		String requiredDimension = (String) elements.get("dimension");
 		boolean wants2D = requiredDimension.equalsIgnoreCase("2D");
 		boolean wants3D = requiredDimension.equalsIgnoreCase("3D");
-		
+
 		String requiredCardinality = (String) elements.get("cardinality");
 		boolean wantsSingle = requiredCardinality.equalsIgnoreCase("single");
 		boolean wantsMultiple = requiredCardinality.equalsIgnoreCase("multiple");
-		
+
 		if ((has2D && wants2D) && (moleculeCount == 1 && wantsSingle)) {
 			return VALID;
 		}
-		
+
 		if ((has2D && wants2D) && (moleculeCount > 1 && wantsMultiple)) {
 			return VALID;
 		}
-		
+
 		if ((has3D && wants3D) && (moleculeCount == 1 && wantsSingle)) {
 			return VALID;
 		}
