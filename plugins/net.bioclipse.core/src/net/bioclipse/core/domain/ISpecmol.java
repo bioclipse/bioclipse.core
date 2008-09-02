61216e27185bf55aba0c8431bb53544aed920048
shk3 <shk3@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2008-09-02 15:21:35 +0000

    added file for handling assigend spectra
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@6841 fcb5ba71-d80d-0410-8237-ba3920747fcc

---
+++ plugins/net.bioclipse.core/src/net/bioclipse/core/domain/ISpecmol.java
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * Copyright (c) 2008 The Bioclipse Project and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * www.eclipse.org—epl-v10.html <http://www.eclipse.org/legal/epl-v10.html>
+ * 
+ * Contributors:
+ *     Stefan Kuhn
+ *     
+ ******************************************************************************/
+package net.bioclipse.core.domain;
+
+import net.bioclipse.core.business.BioclipseException;
+
+public interface ISpecmol extends IBioObject{
+
+	    /**
+	     * @return the IMolecule serialized to CML
+	     * @throws BioclipseException if CML cannot be returned
+	     */
+	    public String getCML() throws BioclipseException;
+	    
+}
