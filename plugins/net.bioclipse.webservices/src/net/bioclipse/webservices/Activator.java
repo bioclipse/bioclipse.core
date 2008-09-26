e3cd3f47e25ea78bc7481a6aa87395fcc1138e9a
carl_masak <carl_masak@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2008-09-26 13:59:09 +0000

    removed a few tab characters
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@7099 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.webservices/src/net/bioclipse/webservices/Activator.java
+++ plugins/net.bioclipse.webservices/src/net/bioclipse/webservices/Activator.java
@@ -18,68 +18,68 @@ import org.osgi.framework.BundleContext;
 import org.osgi.util.tracker.ServiceTracker;
 
 public class Activator extends AbstractUIPlugin {
-	
-	private static final Logger logger = Logger.getLogger(Activator.class);
-	
-	// The plug-in ID
-	private static final String PLUGIN_ID="net.bioclipse.webservices";
 
-	// The shared instance.
-	private static Activator plugin;
-		
+    private static final Logger logger = Logger.getLogger(Activator.class);
+
+    // The plug-in ID
+    private static final String PLUGIN_ID="net.bioclipse.webservices";
+
+    // The shared instance.
+    private static Activator plugin;
+
     //For Spring
     private ServiceTracker finderTracker;
-	
-	/**
-	 * The constructor.
-	 */
-	public Activator() {
-	}
-	
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
-	 */
-	public void start(BundleContext context) throws Exception {
-		super.start(context);
-		plugin = this;
+
+    /**
+     * The constructor.
+     */
+    public Activator() {
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+     */
+    public void start(BundleContext context) throws Exception {
+        super.start(context);
+        plugin = this;
         finderTracker = new ServiceTracker( context, 
-                IWebservicesManager.class.getName(), 
-                null );
-        
+                                            IWebservicesManager.class.getName(), 
+                                            null );
+
         finderTracker.open();	}
-	
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
-	 */
-	public void stop(BundleContext context) throws Exception {
-		plugin = null;
-		super.stop(context);
-	}
-	
-	/**
-	 * Returns the shared instance
-	 *
-	 * @return the shared instance
-	 */
-	public static Activator getDefault() {
-		return plugin;
-	}
-	
-	/**
-	 * Returns an image descriptor for the image file at the given
-	 * plug-in relative path.
-	 *
-	 * @param path the path
-	 * @return the image descriptor
-	 */
-	public static ImageDescriptor getImageDescriptor(String path) {
-		return imageDescriptorFromPlugin(PLUGIN_ID, path);
-	}
+
+    /*
+     * (non-Javadoc)
+     * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+     */
+    public void stop(BundleContext context) throws Exception {
+        plugin = null;
+        super.stop(context);
+    }
+
+    /**
+     * Returns the shared instance
+     *
+     * @return the shared instance
+     */
+    public static Activator getDefault() {
+        return plugin;
+    }
+
+    /**
+     * Returns an image descriptor for the image file at the given
+     * plug-in relative path.
+     *
+     * @param path the path
+     * @return the image descriptor
+     */
+    public static ImageDescriptor getImageDescriptor(String path) {
+        return imageDescriptorFromPlugin(PLUGIN_ID, path);
+    }
 
     public IWebservicesManager getWebservicesManager() {
-    	IWebservicesManager manager = null;
+        IWebservicesManager manager = null;
         try {
             manager = (IWebservicesManager) finderTracker.waitForService(1000*10);
         } catch (InterruptedException e) {
