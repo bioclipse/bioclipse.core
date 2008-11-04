b3e4e9a5a5de520c1814bc97c0c3bb4c8f9fc96e
jonalv <jonalv@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2008-11-04 10:07:23 +0000

    Removed Java 6 specific annotation
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@7533 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.compute/src/net/bioclipse/compute/business/ComputeManager.java
+++ plugins/net.bioclipse.compute/src/net/bioclipse/compute/business/ComputeManager.java
@@ -2,9 +2,7 @@ package net.bioclipse.compute.business;
 
 public class ComputeManager implements IComputeManager {
 
-	@Override
 	public String getNamespace() {
 		return "net.bioclipse.compute";
 	}
-
 }
