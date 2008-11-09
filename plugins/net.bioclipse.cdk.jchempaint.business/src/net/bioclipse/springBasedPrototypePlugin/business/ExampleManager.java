bf241195079134147ceeb31f758b76b60aa720a9
egonw <egonw@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2008-11-09 13:02:47 +0000

    Copied template for adding the JChemPaint manager
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@7575 fcb5ba71-d80d-0410-8237-ba3920747fcc

---
+++ plugins/net.bioclipse.cdk.jchempaint.business/src/net/bioclipse/springBasedPrototypePlugin/business/ExampleManager.java
@@ -0,0 +1,33 @@
+/*******************************************************************************
+ * Copyright (c) 2007 The Bioclipse Project and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * www.eclipse.org—epl-v10.html <http://www.eclipse.org/legal/epl-v10.html>
+ * 
+ * Contributors:
+ *     Jonathan Alvarsson
+ *     
+ ******************************************************************************/
+package net.bioclipse.springBasedPrototypePlugin.business;
+
+import net.bioclipse.ui.Activator;
+
+/**
+ * ExampleManager. All methods declared here that should be reachable 
+ * in the service object must be declared in the corresponding interface  
+ * 
+ * @author jonalv
+ *
+ */
+public class ExampleManager implements IExampleManager {
+
+    public void example(String ex) {
+        Activator.getDefault().CONSOLE.echo(
+            "ExampleManager.example() called with:" + ex ); 
+    }
+
+    public String getNamespace() {
+        return "example";
+    }
+}
---
+++ plugins/net.bioclipse.cdk.jchempaint.business/src/net/bioclipse/springBasedPrototypePlugin/business/IExampleManager.java
@@ -0,0 +1,39 @@
+/*******************************************************************************
+ * Copyright (c) 2007 The Bioclipse Project and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * www.eclipse.org—epl-v10.html <http://www.eclipse.org/legal/epl-v10.html>
+ * 
+ * Contributors:
+ *     Jonathan Alvarsson
+ *     
+ ******************************************************************************/
+package net.bioclipse.springBasedPrototypePlugin.business;
+
+import net.bioclipse.core.PublishedClass;
+import net.bioclipse.core.PublishedMethod;
+import net.bioclipse.core.Recorded;
+import net.bioclipse.core.business.IBioclipseManager;
+
+/**
+ * Example manager interface defining all methods of the service object
+ * 
+ * @author jonalv
+ *
+ */
+@PublishedClass ( "This is an example manager with one example class." +
+		          "Normally this text should contain some information of " +
+		          "what the manager can be ued for" )
+public interface IExampleManager extends IBioclipseManager {
+
+    /**
+     * example method
+     */
+    @Recorded
+    @PublishedMethod ( params = "String ex", 
+                       methodSummary = "This is an example method. " +
+                       		           "Normally this text should describe " +
+                       		           "what the method does " )
+    public void example(String ex);
+}
