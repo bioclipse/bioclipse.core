934f9d49458b700901fe755dbfa7baafac73cda1
egonw <egonw@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2008-11-12 15:50:52 +0000

    Added test suite for scripting.ui
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@7630 fcb5ba71-d80d-0410-8237-ba3920747fcc

---
+++ plugins/net.bioclipse.scripting.ui.tests/src/net/bioclipse/scripting/ui/tests/JsConsoleManagerTest.java
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ * Copyright (c) 2008 The Bioclipse Project and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Ola Spjuth
+ *     Jonathan Alvarsson
+ *
+ ******************************************************************************/
+package net.bioclipse.scripting.ui.tests;
+
+import net.bioclipse.core.business.IBioclipseManager;
+import net.bioclipse.core.tests.AbstractManagerTest;
+import net.bioclipse.scripting.ui.business.IJsConsoleManager;
+import net.bioclipse.scripting.ui.business.JsConsoleManager;
+
+public class JsConsoleManagerTest extends AbstractManagerTest {
+
+    IJsConsoleManager console;
+
+    //Do not use SPRING OSGI for this manager
+    //since we are only testing the implementations of the manager methods
+    public JsConsoleManagerTest() {
+        console = new JsConsoleManager();
+    }
+
+    public IBioclipseManager getManager() {
+        return console;
+    }
+
+}
