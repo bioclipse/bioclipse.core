f7e65f179faad7656664e265c4fd1df9e9410b1b
egonw <egonw@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2008-11-12 15:12:57 +0000

    Added two test suites: for cml and ui.business
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@7628 fcb5ba71-d80d-0410-8237-ba3920747fcc

---
+++ plugins/net.bioclipse.ui.business.tests/src/net/bioclipse/ui/business/tests/CoverageTest.java
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ * Copyright (c) 2008  Egon Willighagen <egonw@users.sf.net>
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contact: Bioclipse Project <http://www.bioclipse.net>
+ ******************************************************************************/
+package net.bioclipse.ui.business.tests;
+
+import net.bioclipse.core.business.IBioclipseManager;
+import net.bioclipse.core.tests.coverage.AbstractCoverageTest;
+import net.bioclipse.ui.business.UIManager;
+
+/**
+ * JUnit tests for checking if the tested Manager is properly tested.
+ * 
+ * @author egonw
+ */
+public class CoverageTest extends AbstractCoverageTest {
+    
+    private static UIManager manager = new UIManager();
+
+    public IBioclipseManager getManager() {
+        return manager;
+    }
+
+}
---
+++ plugins/net.bioclipse.ui.business.tests/src/net/bioclipse/ui/business/tests/JmolManagerTest.java
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ * Copyright (c) 2008 The Bioclipse Project and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Ola Spjuth
+ *     Jonathan Alvarsson
+ *
+ ******************************************************************************/
+package net.bioclipse.ui.business.tests;
+
+import net.bioclipse.core.business.IBioclipseManager;
+import net.bioclipse.core.tests.AbstractManagerTest;
+import net.bioclipse.ui.business.IUIManager;
+import net.bioclipse.ui.business.UIManager;
+
+public class JmolManagerTest extends AbstractManagerTest {
+
+    IUIManager ui;
+
+    //Do not use SPRING OSGI for this manager
+    //since we are only testing the implementations of the manager methods
+    public JmolManagerTest() {
+        ui = new UIManager();
+    }
+
+    public IBioclipseManager getManager() {
+        return ui;
+    }
+
+}
