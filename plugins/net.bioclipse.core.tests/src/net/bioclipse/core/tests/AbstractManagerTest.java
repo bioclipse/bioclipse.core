315d2630f0dc49ecca37e99a32f0a6cc4e728549
egonw <egonw@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2008-11-12 12:47:19 +0000

    Added second API test for managers (addresses bug #112)
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@7620 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.core.tests/src/net/bioclipse/core/tests/AbstractManagerTest.java
+++ plugins/net.bioclipse.core.tests/src/net/bioclipse/core/tests/AbstractManagerTest.java
@@ -16,8 +16,10 @@ import java.lang.reflect.Method;
 import net.bioclipse.core.PublishedMethod;
 import net.bioclipse.core.Recorded;
 import net.bioclipse.core.business.IBioclipseManager;
+import net.bioclipse.core.jobs.Job;
 
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -67,6 +69,75 @@ public abstract class AbstractManagerTest {
                 }
             }
         }
+    }
+
+    /**
+     * If a {@link IBioclipseManager} method <code>foo(IFile)</code> exists,
+     * then there must also be a method in the matching
+     * {@link BioclipseManager} which takes an IFile parameter, <code>foo(IFile)</code>,
+     * or a method which takes an IFile, an IProgressMonitor and an IRunnable
+     * parameter, <code>foo(IFile, IProgressMonitor, IRunnable)</code>.
+     */
+    @Test public void testForFooIFileImplementation() {
+        IBioclipseManager manager = getManager();
+        Class managerInterface = getManagerInterface(manager);
+        Method[] methods = managerInterface.getMethods();
+        for (Method method : methods) {
+            Class[] parameters = method.getParameterTypes();
+            if (parameters.length == 1 &&
+                (parameters[0].getName().equals(IFile.class.getName()))) {
+                // OK, found a foo(IFile)
+                boolean foundMatchingImplementation = false;
+                Method[] otherMethods = manager.getClass().getMethods();
+                for (Method otherMethod : methods) {
+                    Class[] otherParameters = otherMethod.getParameterTypes();
+                    if (otherParameters.length == 1 &&
+                        (otherParameters[0].getName().equals(IFile.class.getName()))) {
+                        foundMatchingImplementation = true;
+                    } else if (otherParameters.length == 3 &&
+                        (otherParameters[0].getName().equals(IFile.class.getName())) &&
+                        (otherParameters[1].getName().equals(IProgressMonitor.class.getName())) &&
+                        (otherParameters[2].getName().equals(Runnable.class.getName()))) {
+                        foundMatchingImplementation = true;
+                    }
+                }
+                Assert.assertTrue(
+                    managerInterface.getName() + " method " + method.getName() +
+                    "(IFile) does not have the required matching " +
+                    manager.getClass().getName() + " method " +
+                    method.getName() +"(IFile) or " +
+                    method.getName() +"(IFile, IProgressMonitor, IRunnable).",
+                    foundMatchingImplementation
+                );
+            }
+        }
+    }
+
+    private Class getManagerInterface(IBioclipseManager manager) {
+        Class[] interfaces = manager.getClass().getInterfaces();
+        String managerName = manager.getClass().getName();
+        String managerPkg = managerName.substring(0, managerName.lastIndexOf('.'));
+        String managerClass = managerName.substring(managerPkg.length()+1);
+        String expectedName = managerPkg + ".I" + managerClass;
+        for (Class interfaz : interfaces) {
+            System.out.println("Expected name " + expectedName);
+            if (interfaz.getName().equals(expectedName))
+                return interfaz;
+        }
+        return null;
+    }
+
+    /**
+     * Tests if the Method has {@link Job} annotation.
+     */
+    private boolean isJob(Method method) {
+        Annotation[] otherAnnots = method.getAnnotations();
+        for (Annotation annot : otherAnnots) {
+            if (annot instanceof Job) {
+                return true;
+            }
+        }
+        return false;
     }
     
     /**
