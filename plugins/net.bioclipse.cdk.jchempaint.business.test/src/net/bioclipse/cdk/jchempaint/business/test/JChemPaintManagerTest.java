0b3d06c45c347ff2f8c50bd46355fa3ca8c97bf4
egonw <egonw@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2008-11-13 12:20:52 +0000

    Added test method to ensure all script methods are available from the manager
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@7656 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.cdk.jchempaint.business.test/src/net/bioclipse/cdk/jchempaint/business/test/JChemPaintManagerTest.java
+++ plugins/net.bioclipse.cdk.jchempaint.business.test/src/net/bioclipse/cdk/jchempaint/business/test/JChemPaintManagerTest.java
@@ -12,10 +12,15 @@
  ******************************************************************************/
 package net.bioclipse.cdk.jchempaint.business.test;
 
+import java.lang.reflect.Method;
+
 import net.bioclipse.cdk.jchempaint.business.IJChemPaintManager;
 import net.bioclipse.cdk.jchempaint.business.JChemPaintManager;
 import net.bioclipse.core.business.IBioclipseManager;
 import net.bioclipse.core.tests.AbstractManagerTest;
+
+import org.junit.Assert;
+import org.junit.Test;
 
 public class JChemPaintManagerTest extends AbstractManagerTest {
 
@@ -29,6 +34,42 @@ public class JChemPaintManagerTest extends AbstractManagerTest {
 
     public IBioclipseManager getManager() {
         return cdk;
+    }
+    
+    @Test public void testImplementsAllControllerHubMethods() throws Exception {
+        Class hub = this.getClass().getClassLoader().loadClass("org.openscience.cdk.controller.IChemModelRelay");
+        Assert.assertNotNull("Could not load the IChemModelRelay", hub);
+        for (Method method : hub.getMethods()) {
+            Method matchingMethod = getMatchingMethod(cdk.getClass(), method);
+            Assert.assertNotNull(
+                "The JChemPaintManager does not implement the IChemModelRelay method " +
+                method.getName(), matchingMethod
+            );
+            Assert.assertEquals(
+                "The IChemModelRelay method " + method.getName() + " must have the return " +
+                "type " + method.getReturnType().getName(),
+                method.getReturnType().getName(),
+                matchingMethod.getReturnType().getName()
+            );
+        }
+    }
+    
+    private Method getMatchingMethod(Class clazz, Method searchedMethod) {
+        for (Method method : clazz.getMethods()) {
+            if (method.getName().equals(searchedMethod.getName())) {
+                Class[] actualParams = method.getParameterTypes();
+                Class[] searchedParams = searchedMethod.getParameterTypes();
+                if (actualParams.length == searchedParams.length) {
+                    boolean paramsMatch = true;
+                    for (int i=0; i<actualParams.length && paramsMatch; i++) {
+                        if (!actualParams[i].getName().equals(searchedParams[i].getName()))
+                            paramsMatch = false;
+                    }
+                    if (paramsMatch) return method;
+                }
+            }
+        }
+        return null;
     }
 
 }
