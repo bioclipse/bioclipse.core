4b1b0b8b96bec1ae24c9352b19cbd2a4a861bd17
egonw <egonw@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2008-11-14 17:06:12 +0000

    Put CMLXOM in a separate plugin, away from the manager code
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@7707 fcb5ba71-d80d-0410-8237-ba3920747fcc

---
+++ externals/org.xmlcml.cmlxom/src/net/bioclipse/cml/managers/Activator.java
@@ -0,0 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2008 Egon Willighagen
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package net.bioclipse.cml.managers;
+
+import org.eclipse.core.runtime.Plugin;
+
+public class Activator extends Plugin {}
