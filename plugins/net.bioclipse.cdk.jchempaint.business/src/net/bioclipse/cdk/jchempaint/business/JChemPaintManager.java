b41e00687d94071329aa0cba502518b851a148ee
egonw <egonw@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2008-11-14 09:51:58 +0000

    Updated header
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@7680 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.cdk.jchempaint.business/src/net/bioclipse/cdk/jchempaint/business/JChemPaintManager.java
+++ plugins/net.bioclipse.cdk.jchempaint.business/src/net/bioclipse/cdk/jchempaint/business/JChemPaintManager.java
@@ -1,14 +1,14 @@
 /*******************************************************************************
- * Copyright (c) 2007 The Bioclipse Project and others.
+ * Copyright (c) 2007      Jonathan Alvarsson
+ *               2007-2008 Ola Spjuth
+ *               2008      Egon Willighagen
+ *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * www.eclipse.org—epl-v10.html <http://www.eclipse.org/legal/epl-v10.html>
- * 
- * Contributors:
- *     Jonathan Alvarsson
- *     Egon Willighagen
- *     Ola Spjuth
+ *
+ * Contact: http://www.bioclipse.net/
  ******************************************************************************/
 package net.bioclipse.cdk.jchempaint.business;
 
