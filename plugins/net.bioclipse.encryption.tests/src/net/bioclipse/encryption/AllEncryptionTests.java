dc44c06c26016ca1a252a7b3371171c523ab30d8
jonalv <jonalv@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2008-11-14 10:51:45 +0000

    test for net.bioclipse.encryption
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@7686 fcb5ba71-d80d-0410-8237-ba3920747fcc

---
+++ plugins/net.bioclipse.encryption.tests/src/net/bioclipse/encryption/AllEncryptionTests.java
@@ -0,0 +1,26 @@
+/*******************************************************************************
+ * Copyright (c) 2007-2008 The Bioclipse Project and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * <http://www.eclipse.org/legal/epl-v10.html>
+ * 
+ * Contributors:
+ *     Jonathan Alvarsson
+ *     
+ ******************************************************************************/
+package net.bioclipse.encryption;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * @author jonalv
+ *
+ */
+@RunWith(value=Suite.class)
+@SuiteClasses( value = { EncryptedPasswordTest.class, } )
+public class AllEncryptionTests {
+
+}
---
+++ plugins/net.bioclipse.encryption.tests/src/net/bioclipse/encryption/EncryptedPasswordTest.java
@@ -0,0 +1,37 @@
+/*******************************************************************************
+ * Copyright (c) 2007-2008 The Bioclipse Project and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * <http://www.eclipse.org/legal/epl-v10.html>
+ * 
+ * Contributors:
+ *     Jonathan Alvarsson
+ *     
+ ******************************************************************************/
+package net.bioclipse.encryption;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+/**
+ * @author jonalv
+ *
+ */
+public class EncryptedPasswordTest {
+
+    @Test
+    public void test() throws Exception {
+
+        final String PSWD = "secretest password"; 
+        EncryptedPassword encryptedPassword 
+            = EncryptedPassword.fromPlainTextPasswordString( PSWD );
+        String pswd = encryptedPassword.toString();
+        
+        encryptedPassword = EncryptedPassword
+                                .fromEncryptedPasswordString( pswd );
+        assertTrue( encryptedPassword.matches( PSWD ) );
+    }
+}
