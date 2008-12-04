d7186bb707b674b1ff6d16eb49a0238bb6f1b180
edrin_t <edrin_t@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2008-12-04 18:22:49 +0000

    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@7967 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.webservices/jars/axis-1_4-stubgenerator.bat
+++ plugins/net.bioclipse.webservices/jars/axis-1_4-stubgenerator.bat
@@ -1,7 +1,7 @@
 echo off
-set TARGETDIR=..\src-axis1
+set TARGETDIR=src-dump-axis1
 set AXIS_LIB=axis-1_4\lib
-set LOG_LIB=..\..\com.tools.logging\jar
+set LOG_LIB=.\..\..\net.bioclipse.logger\log4j\log4j-1.2.15.jar
 set AXISCP=.;%AXIS_LIB%\axis.jar
 set AXISCP=%AXISCP%;%AXIS_LIB%\commons-discovery-0.2.jar
 set AXISCP=%AXISCP%;%AXIS_LIB%\jaxrpc.jar
@@ -9,5 +9,7 @@ set AXISCP=%AXISCP%;%AXIS_LIB%\saaj.jar
 set AXISCP=%AXISCP%;%AXIS_LIB%\wsdl4j-1.5.1.jar
 set AXISCP=%AXISCP%;%AXIS_LIB%\commons-logging-1.0.4.jar
 set AXISCP=%AXISCP%;%AXIS_LIB%\log4j-1.2.8.jar
+set AXISCP=%AXISCP%;%AXIS_LIB%\commons-logging-1.0.4.jar
 echo on
+echo %AXISCP%
 java -cp %AXISCP% org.apache.axis.wsdl.WSDL2Java -o %TARGETDIR% --wrapArrays --timeout -1 %*
