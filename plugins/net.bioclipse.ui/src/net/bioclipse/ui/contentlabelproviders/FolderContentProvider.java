5f1cd30f0be4d86500d4a4c2a21879fd4b0bdc59
shk3 <shk3@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2008-12-17 10:53:29 +0000

    added some general file browsing classes
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@8068 fcb5ba71-d80d-0410-8237-ba3920747fcc

---
+++ plugins/net.bioclipse.ui/src/net/bioclipse/ui/contentlabelproviders/FolderContentProvider.java
@@ -0,0 +1,119 @@
+/*****************************************************************************
+ * Copyright (c) 2008 Bioclipse Project
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ *****************************************************************************/
+
+package net.bioclipse.ui.contentlabelproviders;
+
+import java.util.ArrayList;
+
+import net.bioclipse.core.util.LogUtils;
+
+import org.apache.log4j.Logger;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jface.viewers.ITreeContentProvider;
+import org.eclipse.jface.viewers.Viewer;
+
+/** 
+ * A class implementing ITreeContentProvider and only returning child elements which are 
+ * folders. This can be used to build TreeViewers for browsing for folders (e. g. if the user needs to
+ * decide for a new file).
+ *
+ */
+public class FolderContentProvider implements ITreeContentProvider {
+
+  private static final Logger logger = Logger.getLogger(FolderContentProvider.class);
+    
+	public FolderContentProvider() {
+	}
+
+	public void dispose() {
+
+	}
+
+	public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+	}
+
+	public Object[] getElements(Object inputElement) {
+		return getChildren(inputElement);
+	}
+
+	public Object[] getChildren(Object parentElement) {
+		ArrayList<IResource> childElements = new ArrayList<IResource>();
+		if(parentElement instanceof IFolder){
+			IFolder element = (IFolder) parentElement;
+			try {
+				for(int i=0;i<element.members().length;i++){
+					if(element.members()[i] instanceof IFolder)
+						childElements.add((IFolder)element.members()[i]);
+				}
+			} catch (CoreException e) {
+			    LogUtils.handleException(e,logger);
+			}
+		}else if(parentElement instanceof IWorkspaceRoot){
+			IWorkspaceRoot element = (IWorkspaceRoot) parentElement;
+			try {
+				for(int i=0;i<element.members().length;i++){
+					Object[] elements=element.members();
+					if(elements[i] instanceof IProject)
+						childElements.add((IProject)elements[i]);
+				}
+			} catch (CoreException e) {
+			    LogUtils.handleException(e,logger);
+			}
+		} else if(parentElement instanceof IProject){
+			IProject element = (IProject) parentElement;
+			try {
+				for(int i=0;i<element.members().length;i++){
+					if(element.members()[i] instanceof IFolder)
+						childElements.add((IFolder)element.members()[i]);
+				}
+			} catch (CoreException e) {
+			    LogUtils.handleException(e,logger);
+			}
+		}
+		return childElements.toArray();
+	}
+
+	public Object getParent(Object element) {
+		return ((IFolder)element).getParent();
+	}
+
+	public boolean hasChildren(Object element) {
+		if(element instanceof IFolder){
+			IFolder folder=(IFolder)element;
+			try{
+				IResource[] children=folder.members();
+				for(int i=0;i<children.length;i++){
+					if(children[i] instanceof IFolder)
+						return true;
+				}
+			}catch(CoreException ex){
+			    LogUtils.handleException(ex,logger);
+			}
+		}else 		if(element instanceof IProject){
+			IProject folder=(IProject)element;
+			try{
+			    if (folder.isOpen()) {
+			        IResource[] children=folder.members();
+			        for(int i=0;i<children.length;i++){
+			            if(children[i] instanceof IFolder)
+			                return true;
+			        }
+			    }
+			}catch(CoreException ex){
+				LogUtils.handleException(ex,logger);
+			}
+		}
+		return false;
+	}
+
+}
---
+++ plugins/net.bioclipse.ui/src/net/bioclipse/ui/contentlabelproviders/FolderLabelProvider.java
@@ -0,0 +1,51 @@
+/*****************************************************************************
+ * Copyright (c) 2008 Bioclipse Project
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ *****************************************************************************/
+
+package net.bioclipse.ui.contentlabelproviders;
+
+import org.eclipse.core.resources.IResource;
+import org.eclipse.jface.viewers.ILabelProvider;
+import org.eclipse.jface.viewers.ILabelProviderListener;
+import org.eclipse.swt.graphics.Image;
+
+/** 
+ * A class implementing ILabelProvider and returning the name for text if
+ * the element is any resource. This can be used to build TreeViewers for browsing for any sort of files.
+ *
+ */
+public class FolderLabelProvider implements ILabelProvider {
+
+	public FolderLabelProvider() {
+	}
+
+	public void addListener(ILabelProviderListener listener) {
+	}
+
+	public void dispose() {
+	}
+
+	public boolean isLabelProperty(Object element, String property) {
+		return false;
+	}
+
+	public void removeListener(ILabelProviderListener listener) {
+	}
+
+	public Image getImage(Object element) {
+		return null;
+	}
+
+	public String getText(Object element) {
+		if (element instanceof IResource) {
+			return ((IResource)element).getName();
+		}else{
+			return "Unknown element";
+		}
+	}
+}
