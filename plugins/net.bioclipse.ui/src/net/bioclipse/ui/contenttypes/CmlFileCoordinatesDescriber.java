8e723db634f469f6bf4658c8a7c548f7e6c387c9
gilleain <gilleain@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-02-23 12:01:43 +0000

    Copyright notices.
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@8976 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.ui/src/net/bioclipse/ui/contenttypes/CmlFileCoordinatesDescriber.java
+++ plugins/net.bioclipse.ui/src/net/bioclipse/ui/contenttypes/CmlFileCoordinatesDescriber.java
@@ -1,3 +1,16 @@
+/*******************************************************************************
+ * Copyright (c) 2008 The Bioclipse Project and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Ola Spjuth
+ *     Stefan Kuhn
+ *     Carl MŠsak
+ *     
+ ******************************************************************************/
 package net.bioclipse.ui.contenttypes;
 
 import java.io.IOException;
