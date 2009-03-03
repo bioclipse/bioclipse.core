c49fd9eb4060e2665e1cb5c6d27480671cdbd74f
carl_masak <carl_masak@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-03-03 16:44:11 +0000

    [scripting] added R Console and Jmol Console
    These don't do anything yet. Still need to plug them in.
    
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@9201 fcb5ba71-d80d-0410-8237-ba3920747fcc

---
+++ plugins/net.bioclipse.scripting.ui/src/net/bioclipse/scripting/ui/views/JmolConsoleView.java
@@ -0,0 +1,11 @@
+package net.bioclipse.scripting.ui.views;
+
+
+public class JmolConsoleView extends ScriptingConsoleView {
+
+    @Override
+    protected String executeCommand( String command ) {
+        return "Not implemented yet";
+    }
+
+}
---
+++ plugins/net.bioclipse.scripting.ui/src/net/bioclipse/scripting/ui/views/RConsoleView.java
@@ -0,0 +1,11 @@
+package net.bioclipse.scripting.ui.views;
+
+
+public class RConsoleView extends ScriptingConsoleView {
+
+    @Override
+    protected String executeCommand( String command ) {
+        return "Not implemented yet";
+    }
+
+}
