248c95f8b3a7de45cdc23c537bd198ade2deb822
carl_masak <carl_masak@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-03-03 16:45:55 +0000

    [scripting] wired up the Jmol console
    Theoretically, it shoudl work now.
    
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@9202 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.scripting.ui/src/net/bioclipse/scripting/ui/views/JmolConsoleView.java
+++ plugins/net.bioclipse.scripting.ui/src/net/bioclipse/scripting/ui/views/JmolConsoleView.java
@@ -1,11 +1,14 @@
 package net.bioclipse.scripting.ui.views;
 
+import net.bioclipse.scripting.ui.Activator;
+
 
 public class JmolConsoleView extends ScriptingConsoleView {
 
     @Override
     protected String executeCommand( String command ) {
-        return "Not implemented yet";
+        Activator.getDefault().getJmolManager().run( command );
+        return null;
     }
 
 }
