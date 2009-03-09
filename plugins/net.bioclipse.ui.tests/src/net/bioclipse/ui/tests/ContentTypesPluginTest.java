57afe779c480e8d7bc679c8efbb08bfec5937aa4
shk3 <shk3@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-03-09 14:01:53 +0000

    tests work
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@9263 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.ui.tests/src/net/bioclipse/ui/tests/ContentTypesPluginTest.java
+++ plugins/net.bioclipse.ui.tests/src/net/bioclipse/ui/tests/ContentTypesPluginTest.java
@@ -13,11 +13,14 @@
 package net.bioclipse.ui.tests;
 
 import java.io.InputStream;
+import java.net.URI;
+import java.net.URL;
 
 import junit.framework.Assert;
 import net.bioclipse.core.MockIFile;
 
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.content.IContentType;
 import org.eclipse.core.runtime.content.IContentTypeManager;
@@ -27,57 +30,69 @@ import org.junit.Test;
 public class ContentTypesPluginTest{
  
     @Test public void testCMLSingle2d() throws Exception{
-        String path = getClass().getResource("/testFiles/single2d.cml").getPath();
+        URI uri = getClass().getResource("/testFiles/single2d.cml").toURI();
+        URL url=FileLocator.toFileURL(uri.toURL());
+        String path=url.getFile();
         IFile file=new MockIFile(path);
         IContentTypeManager contentTypeManager = Platform.getContentTypeManager();
         InputStream stream = file.getContents();
-        IContentType contentType = contentTypeManager.findContentTypeFor(stream, file.getName());
+        IContentType contentType = contentTypeManager.findContentTypeFor(stream, "single2d.cml");
         stream.close();
-        Assert.assertEquals( "net.bioclipse.contenttypes.cml.singleMolecule2d",contentType.getName() );
+        Assert.assertEquals( "net.bioclipse.contenttypes.cml.singleMolecule2d",contentType.getId() );
     }
     @Test public void testCMLSingle3d() throws Exception{
-        String path = getClass().getResource("/testFiles/single3d.cml").getPath();
+        URI uri = getClass().getResource("/testFiles/single3d.cml").toURI();
+        URL url=FileLocator.toFileURL(uri.toURL());
+        String path=url.getFile();
         IFile file=new MockIFile(path);
         IContentTypeManager contentTypeManager = Platform.getContentTypeManager();
         InputStream stream = file.getContents();
-        IContentType contentType = contentTypeManager.findContentTypeFor(stream, file.getName());
+        IContentType contentType = contentTypeManager.findContentTypeFor(stream, "single3d.cml");
         stream.close();
-        Assert.assertEquals( "net.bioclipse.contenttypes.cml.singleMolecule3d",contentType.getName() );
+        Assert.assertEquals( "net.bioclipse.contenttypes.cml.singleMolecule3d",contentType.getId() );
     }
     @Test public void testCMLSingle5d() throws Exception{
-        String path = getClass().getResource("/testFiles/single5d.cml").getPath();
+        URI uri = getClass().getResource("/testFiles/single5d.cml").toURI();
+        URL url=FileLocator.toFileURL(uri.toURL());
+        String path=url.getFile();
         IFile file=new MockIFile(path);
         IContentTypeManager contentTypeManager = Platform.getContentTypeManager();
         InputStream stream = file.getContents();
-        IContentType contentType = contentTypeManager.findContentTypeFor(stream, file.getName());
+        IContentType contentType = contentTypeManager.findContentTypeFor(stream, "single5d.cml");
         stream.close();
-        Assert.assertEquals( "net.bioclipse.contenttypes.cml.singleMolecule5d",contentType.getName() );
+        Assert.assertEquals( "net.bioclipse.contenttypes.cml.singleMolecule5d",contentType.getId() );
     }
     @Test public void testCMLMultipleSingle2d() throws Exception{
-        String path = getClass().getResource("/testFiles/multiple2d.cml").getPath();
+        URI uri = getClass().getResource("/testFiles/multiple2d.cml").toURI();
+        URL url=FileLocator.toFileURL(uri.toURL());
+        String path=url.getFile();
         IFile file=new MockIFile(path);
         IContentTypeManager contentTypeManager = Platform.getContentTypeManager();
         InputStream stream = file.getContents();
-        IContentType contentType = contentTypeManager.findContentTypeFor(stream, file.getName());
+        IContentType contentType = contentTypeManager.findContentTypeFor(stream, "multiple2d.cml");
         stream.close();
-        Assert.assertEquals( "net.bioclipse.contenttypes.cml.multipleMolecule2d",contentType.getName() );
+        Assert.assertEquals( "net.bioclipse.contenttypes.cml.multipleMolecule2d",contentType.getId() );
     }
     @Test public void testCMLMultipleSingle3d() throws Exception{
-        String path = getClass().getResource("/testFiles/multiple3d.cml").getPath();
+        URI uri = getClass().getResource("/testFiles/multiple3d.cml").toURI();
+        URL url=FileLocator.toFileURL(uri.toURL());
+        String path=url.getFile();
         IFile file=new MockIFile(path);
         IContentTypeManager contentTypeManager = Platform.getContentTypeManager();
         InputStream stream = file.getContents();
-        IContentType contentType = contentTypeManager.findContentTypeFor(stream, file.getName());
+        IContentType contentType = contentTypeManager.findContentTypeFor(stream, "multiple3d.cml");
         stream.close();
-        Assert.assertEquals( "net.bioclipse.contenttypes.cml.multipleMolecule3d",contentType.getName() );
+        Assert.assertEquals( "net.bioclipse.contenttypes.cml.multipleMolecule3d",contentType.getId() );
     }
     @Test public void testCMLMultipleSingle5d() throws Exception{
-        String path = getClass().getResource("/testFiles/multiple5d.cml").getPath();
+        URI uri = getClass().getResource("/testFiles/multiple5d.cml").toURI();
+        URL url=FileLocator.toFileURL(uri.toURL());
+        String path=url.getFile();
         IFile file=new MockIFile(path);
         IContentTypeManager contentTypeManager = Platform.getContentTypeManager();
         InputStream stream = file.getContents();
-        IContentType contentType = contentTypeManager.findContentTypeFor(stream, file.getName());
+        IContentType contentType = contentTypeManager.findContentTypeFor(stream, "multiple5d.cml");
         stream.close();
-        Assert.assertEquals( "net.bioclipse.contenttypes.cml.multipleMolecule5d",contentType.getName() );
+        Assert.assertEquals( "net.bioclipse.contenttypes.cml.multipleMolecule5d",contentType.getId() );
     }
 }
