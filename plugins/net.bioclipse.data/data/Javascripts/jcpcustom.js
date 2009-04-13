1eb5e763d8fadf23d68b54b34a003fa32b1ef744
ospjuth <ospjuth@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-04-13 16:59:14 +0000

    Added a sample script to demonstrate JCP scripting
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@9774 fcb5ba71-d80d-0410-8237-ba3920747fcc

---
+++ plugins/net.bioclipse.data/data/Javascripts/jcpcustom.js
@@ -0,0 +1,5 @@
+//Create a molecule from SMILES, open in JCP and customize rendering
+mol=cdk.fromSMILES("C1CCCCC1CC(CCO)CCNC")
+mol2=cdk.generate2dCoordinates(mol)
+ui.open(mol2)
+jcpprop.setShowAromaticity(true)
