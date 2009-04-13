5eb242d0bb1ffd52e1197667720efb9cb35fbad0
ospjuth <ospjuth@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-04-13 13:18:48 +0000

    Added documentation for JS samples in sample data.
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@9760 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.data/data/Javascripts/extractsmiles.js
+++ plugins/net.bioclipse.data/data/Javascripts/extractsmiles.js
@@ -1,3 +1,4 @@
+//Output SMILES for all molecules in an SDFile
 var mols = cdk.loadMolecules("/Sample Data/SDF/Fragments2.sdf");
 for (var i = 0; i < mols.size(); i++) {
 	js.print(mols.get(i).getSMILES() + "\n");
