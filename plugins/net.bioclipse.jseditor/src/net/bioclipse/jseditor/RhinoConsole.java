f3fb7853d9fbfa012af940b139ad6d0c3ec0311b
carl_masak <carl_masak@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-04-14 10:33:24 +0000

    [RhinoConsole] whitespace therapy
    - introduced empty lines between methods
    - shortened lines to be within 80 chars
    - intented things consistently and without tab characters
    - some vertical alignment
    
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@9780 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.jseditor/src/net/bioclipse/jseditor/RhinoConsole.java
+++ plugins/net.bioclipse.jseditor/src/net/bioclipse/jseditor/RhinoConsole.java
@@ -31,114 +31,133 @@ import org.eclipse.ui.console.IConsoleFactory;
  *    Johannes Wagener - initial API and implementation
  */
 public class RhinoConsole implements IConsoleFactory {
-        private static MessageConsole messageConsole = null;
-        private static String consoleName = "Rhino Javascript Console";
-        private static MessageConsoleStream out = null,
-                                                                                out_blue = null,
-                                                                                out_red = null;
-        public void openConsole() {
-                show();
-        }
-        public static void show() {
-                IWorkbench wb = PlatformUI.getWorkbench();
-                IWorkbenchPage wbPage = wb.getActiveWorkbenchWindow().getActivePage(); 
-        if (wbPage != null) {
-        	try {
-        		IConsoleView conView = (IConsoleView) wbPage.showView(
-                                                IConsoleConstants.ID_CONSOLE_VIEW);
-        		conView.display(getRhinoConsole());
-        	} catch (PartInitException e) {
-        		PluginLogger.log("Console.show() - PartInitException: " + e.getMessage());
-        	}
-        }
-        }
-        public static MessageConsole getRhinoConsole() {
-                if (messageConsole == null) {
-                        messageConsole = findConsole(consoleName);
-                }
-                return messageConsole;
-        }
-        private static MessageConsole findConsole(String name) {
-                ConsolePlugin conPlugin = ConsolePlugin.getDefault();
-                IConsoleManager conManager = conPlugin.getConsoleManager();
-                IConsole[] consAll = conManager.getConsoles();
-                for (int i = 0; i < consAll.length; i++)
-                        if (name.equals(consAll[i].getName()))
-                                return (MessageConsole) consAll[i];
-                //no console found, so we create a new one
-                MessageConsole rhinoConsole = new MessageConsole(name, null);
-                conManager.addConsoles(new IConsole[]{rhinoConsole});
-                return rhinoConsole;
-        }
-        private static void println(final MessageConsoleStream consolestream, final String message) {
-        	int message_length = message.length();
-        	final int MAX_SIZE = 25000;
-        	if (message_length > MAX_SIZE) {
-        		Runnable r = new Runnable() {
-                    public void run() {
-                    	consolestream.print(message.substring(0, MAX_SIZE));
-                    	consolestream.println(" [...]");
-                    	consolestream.println(" A String was cut: the String exceeded the maxiumum size of 25000");
-                    }
-        		};
-        		Display.getDefault().asyncExec(r);
-        	} else {
-        		Runnable r = new Runnable() {
-                    public void run() {
-                    	consolestream.println(message);
-                    }
-        		};
-        		Display.getDefault().asyncExec(r);
-        	}
-        }
-        
-        public static void writeToConsole(final String message) {
-        	println(getConsoleStream(), message);
+    private static MessageConsole messageConsole = null;
+    private static String consoleName = "Rhino Javascript Console";
+    private static MessageConsoleStream out = null,
+                                        out_blue = null,
+                                        out_red = null;
 
+    public void openConsole() {
+        show();
+    }
+
+    public static void show() {
+        IWorkbench wb = PlatformUI.getWorkbench();
+        IWorkbenchPage wbPage = wb.getActiveWorkbenchWindow().getActivePage(); 
+        if (wbPage != null) {
+            try {
+                IConsoleView conView = (IConsoleView) wbPage.showView(
+                        IConsoleConstants.ID_CONSOLE_VIEW);
+                conView.display(getRhinoConsole());
+            } catch (PartInitException e) {
+                PluginLogger.log("Console.show() - PartInitException: "
+                                 + e.getMessage());
+            }
         }
-        public static void writeToConsoleBlue(final String message) {
-        	println(getConsoleStreamBlue(), message);
+    }
+
+    public static MessageConsole getRhinoConsole() {
+        if (messageConsole == null) {
+            messageConsole = findConsole(consoleName);
         }
-        public static void writeToConsoleRed(final String message) {
-        	println(getConsoleStreamRed(), message);
-        }
-        // with time-stamp
-        public static void writeToConsoleBlueT(String message) {
-                writeToConsoleBlue(getCurrentTime() + " " + message);
-        }
-        // with time-stamp
-        public static void writeToConsoleT(String message) {
-                writeToConsole(getCurrentTime() + " " + message);
-        }
-        // with time-stamp
-        public static void writeToConsoleRedT(String message) {
-                writeToConsoleRed(getCurrentTime() + " " + message);
-        }
-        private static MessageConsoleStream getConsoleStream() {
-                if (out == null)
-                        out = getRhinoConsole().newMessageStream();
-                return out;
-        }
-        private static MessageConsoleStream getConsoleStreamBlue() {
-                if (out_blue == null) {
-                        Color color_blue = PlatformUI.getWorkbench().getDisplay().getSystemColor(SWT.COLOR_BLUE);
-                        out_blue = getRhinoConsole().newMessageStream();
-                        out_blue.setColor(color_blue);
+        return messageConsole;
+    }
+
+    private static MessageConsole findConsole(String name) {
+        ConsolePlugin conPlugin = ConsolePlugin.getDefault();
+        IConsoleManager conManager = conPlugin.getConsoleManager();
+        IConsole[] consAll = conManager.getConsoles();
+        for (int i = 0; i < consAll.length; i++)
+            if (name.equals(consAll[i].getName()))
+                return (MessageConsole) consAll[i];
+        //no console found, so we create a new one
+        MessageConsole rhinoConsole = new MessageConsole(name, null);
+        conManager.addConsoles(new IConsole[]{rhinoConsole});
+        return rhinoConsole;
+    }
+
+    private static void println(final MessageConsoleStream consolestream,
+                                final String message) {
+        int message_length = message.length();
+        final int MAX_SIZE = 25000;
+        if (message_length > MAX_SIZE) {
+            Runnable r = new Runnable() {
+                public void run() {
+                    consolestream.print(message.substring(0, MAX_SIZE));
+                    consolestream.println(" [...]");
+                    consolestream.println(" A String was truncated: the String "
+                                          + "exceeded the maxiumum size of "
+                                          + "25000");
                 }
-                return out_blue;
-        }
-        private static MessageConsoleStream getConsoleStreamRed() {
-                if (out_red == null) {
-                        Color color_red = PlatformUI.getWorkbench().getDisplay().getSystemColor(SWT.COLOR_RED);
-                        out_red = getRhinoConsole().newMessageStream();
-                        out_red.setColor(color_red);
+            };
+            Display.getDefault().asyncExec(r);
+        } else {
+            Runnable r = new Runnable() {
+                public void run() {
+                    consolestream.println(message);
                 }
-                return out_red;
+            };
+            Display.getDefault().asyncExec(r);
         }
-        private static String getCurrentTime() {
-                SimpleDateFormat simpleDateForm = new SimpleDateFormat("hh:mm:ss");
-                Date current = new Date();
-                current.setTime(System.currentTimeMillis());
-                return simpleDateForm.format(current);
+    }
+
+    public static void writeToConsole(final String message) {
+        println(getConsoleStream(), message);
+    }
+
+    public static void writeToConsoleBlue(final String message) {
+        println(getConsoleStreamBlue(), message);
+    }
+
+    public static void writeToConsoleRed(final String message) {
+        println(getConsoleStreamRed(), message);
+    }
+
+    // with time-stamp
+    public static void writeToConsoleBlueT(String message) {
+        writeToConsoleBlue(getCurrentTime() + " " + message);
+    }
+
+    // with time-stamp
+    public static void writeToConsoleT(String message) {
+        writeToConsole(getCurrentTime() + " " + message);
+    }
+
+    // with time-stamp
+    public static void writeToConsoleRedT(String message) {
+        writeToConsoleRed(getCurrentTime() + " " + message);
+    }
+
+    private static MessageConsoleStream getConsoleStream() {
+        if (out == null)
+            out = getRhinoConsole().newMessageStream();
+        return out;
+    }
+
+    private static MessageConsoleStream getConsoleStreamBlue() {
+        if (out_blue == null) {
+            Color color_blue = PlatformUI.getWorkbench().getDisplay()
+                               .getSystemColor(SWT.COLOR_BLUE);
+            out_blue = getRhinoConsole().newMessageStream();
+            out_blue.setColor(color_blue);
         }
+        return out_blue;
+    }
+
+    private static MessageConsoleStream getConsoleStreamRed() {
+        if (out_red == null) {
+            Color color_red = PlatformUI.getWorkbench().getDisplay()
+                              .getSystemColor(SWT.COLOR_RED);
+            out_red = getRhinoConsole().newMessageStream();
+            out_red.setColor(color_red);
+        }
+        return out_red;
+    }
+
+    private static String getCurrentTime() {
+        SimpleDateFormat simpleDateForm = new SimpleDateFormat("hh:mm:ss");
+        Date current = new Date();
+        current.setTime(System.currentTimeMillis());
+        return simpleDateForm.format(current);
+    }
 }
