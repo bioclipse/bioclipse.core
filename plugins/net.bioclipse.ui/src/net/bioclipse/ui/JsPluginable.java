8b6722382e73a26384be3efed0771ea9a49fe150
carl_masak <carl_masak@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-04-15 07:53:12 +0000

    [net.bioclipse.r] reinstated js injection in R
    You can now write
    
    > `answer` * 100
    [1] 4200
    
    and have the js variable 'answer' be interpolated into the R expression.
    Fixes bug #856.
    
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@9809 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.ui/src/net/bioclipse/ui/JsPluginable.java
+++ plugins/net.bioclipse.ui/src/net/bioclipse/ui/JsPluginable.java
@@ -9,13 +9,13 @@
 package net.bioclipse.ui;
 
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.List;
 
 import net.bioclipse.scripting.OutputProvider;
 
 public interface JsPluginable {
 
-    void eval(ArrayList<Object> al) throws IOException;
+    void eval(List<Object> al) throws IOException;
 
     void setOutputProvider(OutputProvider outputProvider);
 }
