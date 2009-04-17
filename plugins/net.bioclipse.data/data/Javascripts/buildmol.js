4137c9d1889918ec5cad07031f87c0250ba2dbc1
ospjuth <ospjuth@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-04-17 06:48:38 +0000

    Updated buildmol.js in sampledata to gist fork 96594
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@9864 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.data/data/Javascripts/buildmol.js
+++ plugins/net.bioclipse.data/data/Javascripts/buildmol.js
@@ -1,34 +1,30 @@
 /*******************************************************************************
- * Bioclipse-script to iteratively build a molecule in JChemPaint
- * Author: Ola Spjuth
- ******************************************************************************/
-
+* Bioclipse-script to iteratively build a molecule in JChemPaint
+* Author: Ola Spjuth
+* Author: Egon Willighagen
+******************************************************************************/
+ 
 //Start with a carbon and open in JCP
 mol=cdk.fromSMILES("C")
 mol2=cdk.generate2dCoordinates(mol)
 ui.open(mol2)
-
+ 
 //Add some atoms and bonds and wait 1 sec in between
 atom=jcp.getModel().getAtomContainer().getAtom(0);
-jcp.addAtom("C", atom)
+atom=jcp.addAtom("C", atom)
 js.delay(1)
-atom=jcp.getModel().getAtomContainer().getAtom(1);
-jcp.addAtom("C", atom)
+atom=jcp.addAtom("C", atom)
 js.delay(1)
-atom=jcp.getModel().getAtomContainer().getAtom(2);
-jcp.addAtom("C", atom)
+atom=jcp.addAtom("C", atom)
 js.delay(1)
-atom=jcp.getModel().getAtomContainer().getAtom(3);
-jcp.addAtom("C", atom)
+atom=jcp.addAtom("C", atom)
 js.delay(1)
-atom=jcp.getModel().getAtomContainer().getAtom(4);
 jcp.addPhenyl(atom)
 js.delay(1)
-atom=jcp.getModel().getAtomContainer().getAtom(jcp.getModel().getAtomContainer().getAtomCount()-1);
-jcp.addAtom("C", atom)
+molecule=jcp.getModel().getAtomContainer()
+atom=molecule.getAtom(molecule.getAtomCount()-1);
+atom=jcp.addAtom("C", atom)
 js.delay(1)
-atom=jcp.getModel().getAtomContainer().getAtom(jcp.getModel().getAtomContainer().getAtomCount()-1);
-jcp.addAtom("C", atom)
+atom=jcp.addAtom("C", atom)
 js.delay(1)
-atom=jcp.getModel().getAtomContainer().getAtom(jcp.getModel().getAtomContainer().getAtomCount()-1);
 jcp.addRing(atom,5)
