6bee315665e06165fb1ff5264ca1e302a5d8f3a5
goglepox <goglepox@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-05-12 08:46:56 +0000

    Added copyright and moved to net.bc.inchi.ui
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@10243 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.inchi.business/src/net/bioclipse/inchi/ui/InChIDialog.java
+++ plugins/net.bioclipse.inchi.business/src/net/bioclipse/inchi/ui/InChIDialog.java
@@ -1,4 +1,14 @@
-package ui;
+/*******************************************************************************
+ * Copyright (c) 2009  Arvid Berg <goglepox@users.sourceforge.net>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * www.eclipse.org—epl-v10.html <http://www.eclipse.org/legal/epl-v10.html>
+ *
+ * Contact: http://www.bioclipse.net/
+ ******************************************************************************/
+package net.bioclipse.inchi.ui;
 
 import java.util.Map;
 
