4eafe2437fdc06965e3e5a63c898b025bde00748
egonw <egonw@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-05-13 22:59:09 +0000

    Updated script for current API
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@10359 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.data/data/Javascripts/extractsmiles.js
+++ plugins/net.bioclipse.data/data/Javascripts/extractsmiles.js
@@ -1,5 +1,5 @@
 //Output SMILES for all molecules in an SDFile
 var mols = cdk.loadMolecules("/Sample Data/SDF/Fragments2.sdf");
 for (var i = 0; i < mols.size(); i++) {
-	js.print(mols.get(i).getSMILES() + "\n");
+	js.print(cdk.calculateSMILES(mols.get(i)) + "\n");
 }
