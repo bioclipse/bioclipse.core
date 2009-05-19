d301cbc54924d96332bb53cef2dec2a924910c95
egonw <egonw@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-05-19 20:36:42 +0000

    moved pick workspacedialog to ui plugin because that's where it's needed for setting up the workspace before depending code is run.
    Also moved Spring extender starting code from ui to core to no success... :(
    
    From: jonalv <jonalv@fcb5ba71-d80d-0410-8237-ba3920747fcc>
    
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@10575 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.ui/src/net/bioclipse/ui/actions/ActionSwitchWorkspace.java
+++ plugins/net.bioclipse.ui/src/net/bioclipse/ui/actions/ActionSwitchWorkspace.java
@@ -8,7 +8,7 @@
  *******************************************************************************/
 package net.bioclipse.ui.actions;
 
-import net.bioclipse.core.PickWorkspaceDialog;
+import net.bioclipse.ui.dialogs.PickWorkspaceDialog;
 
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.dialogs.Dialog;
