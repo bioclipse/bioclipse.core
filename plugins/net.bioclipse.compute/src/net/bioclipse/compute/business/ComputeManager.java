451abd225c966ea6c2485c4b0f8e41cbf6059b71
shk3 <shk3@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-05-20 11:31:52 +0000

    net.bioclipse.computee is a legacy plugin no longer needed, therefore I delete it.
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@10611 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.compute/src/net/bioclipse/compute/business/ComputeManager.java
+++
@@ -1,8 +0,0 @@
-package net.bioclipse.compute.business;
-
-public class ComputeManager implements IComputeManager {
-
-	public String getNamespace() {
-		return "net.bioclipse.compute";
-	}
-}
--- plugins/net.bioclipse.compute/src/net/bioclipse/compute/business/IComputeManager.java
+++
@@ -1,7 +0,0 @@
-package net.bioclipse.compute.business;
-
-import net.bioclipse.managers.business.IBioclipseManager;
-
-public interface IComputeManager extends IBioclipseManager {
-
-}
