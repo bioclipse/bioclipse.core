aeba75a35d6fec9fb4c0df31ab43a8d507412e03
egonw <egonw@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-05-20 06:47:54 +0000

    Remove debug output to STDOUT
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@10587 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.ui/src/net/bioclipse/ui/ApplicationWorkbenchAdvisor.java
+++ plugins/net.bioclipse.ui/src/net/bioclipse/ui/ApplicationWorkbenchAdvisor.java
@@ -3,7 +3,7 @@
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
- * www.eclipse.org—epl-v10.html <http://www.eclipse.org/legal/epl-v10.html>
+ * http://www.eclipse.org/legal/epl-v10.html.
  * 
  * Contributors:
  *     Ola Spjuth - initial API and implementation
@@ -70,7 +70,6 @@ public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisorHack {
     private boolean abort;
 
     public ApplicationWorkbenchAdvisor() {
-        System.out.println("Yeah, i'm being run!");
         // fetch the Location that we will be modifying 
         Location instanceLoc = Platform.getInstanceLocation();
         // if the location is already set, we start from eclipse
