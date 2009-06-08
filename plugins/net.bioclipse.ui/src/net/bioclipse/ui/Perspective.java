56e1263e1f97eb1bf3b848d81245b22c16a49ef2
ospjuth <ospjuth@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-06-08 07:41:15 +0000

    Commented out redundant ShowViewShortcuts since the views are not available.
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@11111 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.ui/src/net/bioclipse/ui/Perspective.java
+++ plugins/net.bioclipse.ui/src/net/bioclipse/ui/Perspective.java
@@ -87,8 +87,8 @@ public class Perspective implements IPerspectiveFactory {
         layout.addShowViewShortcut(IPageLayout.ID_PROP_SHEET);    
         layout.addShowViewShortcut(IPageLayout.ID_PROGRESS_VIEW);    
         layout.addShowViewShortcut(IPageLayout.ID_OUTLINE);    
-        layout.addShowViewShortcut(IPageLayout.ID_PROBLEM_VIEW);    
-        layout.addShowViewShortcut(IPageLayout.ID_TASK_LIST);    
+//        layout.addShowViewShortcut(IPageLayout.ID_PROBLEM_VIEW);    
+//        layout.addShowViewShortcut(IPageLayout.ID_TASK_LIST);    
 
     }
 }
