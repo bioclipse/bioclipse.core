1aca00b8e237dcb90eb49d09b7d902d9a8b9b23c
ospjuth <ospjuth@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-06-10 21:11:42 +0000

    Implemented PropertyCalculators for the active tests
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@11253 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.ds.signsig.bursi/src/net/bioclipse/ds/signsig/bursi/SignSIgRunner.java
+++ plugins/net.bioclipse.ds.signsig.bursi/src/net/bioclipse/ds/signsig/bursi/SignSIgRunner.java
@@ -41,11 +41,11 @@ import net.bioclipse.core.business.BioclipseException;
 import net.bioclipse.core.domain.IMolecule;
 import net.bioclipse.core.util.LogUtils;
 import net.bioclipse.ds.model.AbstractDSTest;
+import net.bioclipse.ds.model.DSException;
 import net.bioclipse.ds.model.IDSTest;
 import net.bioclipse.ds.model.ITestResult;
 import net.bioclipse.ds.model.SimpleResult;
 import net.bioclipse.ds.model.SubStructureMatch;
-import net.bioclipse.ds.model.impl.DSException;
 
 
 public class SignSIgRunner extends AbstractDSTest implements IDSTest{
