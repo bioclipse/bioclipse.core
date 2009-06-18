f2a067e544facd8ae5db2ab15e7b0dca4ae2a38e
egonw <egonw@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-06-18 06:53:23 +0000

    Added missing copyright/license statements
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@11402 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.gist/src/net/bioclipse/gist/business/IJavaGistManager.java
+++ plugins/net.bioclipse.gist/src/net/bioclipse/gist/business/IJavaGistManager.java
@@ -1,5 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2009  Egon Willighagen <egonw@users.sf.net>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contact: Bioclipse Project <http://www.bioclipse.net>
+ ******************************************************************************/
 package net.bioclipse.gist.business;
-
 
 public interface IJavaGistManager extends IGistManager {
 
