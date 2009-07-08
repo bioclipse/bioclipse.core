f8cb96fa575ea4f12de18c3b0243969350954fef
egonw <egonw@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-07-08 11:18:56 +0000

    Finished interfaces set up of NWO: added missing Java interface
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@11644 fcb5ba71-d80d-0410-8237-ba3920747fcc

---
+++ plugins/net.bioclipse.ui.business/src/net/bioclipse/ui/business/IJavaUIManager.java
@@ -0,0 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2009  Egon Willighagen <egonw@users.sf.net>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * <http://www.eclipse.org/legal/epl-v10.html>
+ *
+ * Contact: http://www.bioclipse.net/
+ ******************************************************************************/
+package net.bioclipse.ui.business;
+
+public interface IJavaUIManager extends IUIManager {
+
+}
