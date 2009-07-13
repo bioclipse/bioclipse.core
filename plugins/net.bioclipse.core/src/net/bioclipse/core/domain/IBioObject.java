520fdb2b0fc23219816d3c37694b224b74050182
Ola Spjuth <ola.spjuth@gmail.com>
2009-07-13 16:36:47 +0200

    [IBioObject] revised JavaDoc
    Added sentence structure, formatting and an indefinite article.

--- plugins/net.bioclipse.core/src/net/bioclipse/core/domain/IBioObject.java
+++ plugins/net.bioclipse.core/src/net/bioclipse/core/domain/IBioObject.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007-2008 The Bioclipse Project and others.
+ * Copyright (c) 2007-2009 The Bioclipse Project and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,6 +8,7 @@
  * Contributors:
  *     Jonathan Alvarsson
  *     Ola Spjuth
+ *     Carl MŠsak
  *     
  ******************************************************************************/
 package net.bioclipse.core.domain;
@@ -24,12 +25,12 @@ import org.eclipse.core.runtime.IAdaptable;
 public interface IBioObject extends IAdaptable {
     
     /**
-     * @return Eclipse resource
+     * @return an Eclipse resource
      */
     public IResource getResource();
     
     /**
-     * Set associated Eclipse Resource for this IBioObject
+     * Associate an Eclipse resource for this <code>IBioObject</code>.
      */
     public void setResource(IResource resource);
     /**
