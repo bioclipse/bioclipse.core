ce9335d379a1f42b057568d642ad98aff12e9d02
Egon Willighagen <egon.willighagen@gmail.com>
2009-09-24 11:31:52 +0200

    Added unit tests for RelaxNG validation

--- plugins/net.bioclipse.xml.test/src/net/bioclipse/xml/test/AbstractXmlManagerPluginTest.java
+++ plugins/net.bioclipse.xml.test/src/net/bioclipse/xml/test/AbstractXmlManagerPluginTest.java
@@ -10,11 +10,22 @@
  ******************************************************************************/
 package net.bioclipse.xml.test;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.List;
+
+import net.bioclipse.core.ResourcePathTransformer;
 import net.bioclipse.core.tests.AbstractManagerTest;
 import net.bioclipse.managers.business.IBioclipseManager;
 import net.bioclipse.ui.business.UIManager;
 import net.bioclipse.xml.business.IXmlManager;
+import net.bioclipse.xml.business.XMLError;
 
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -26,8 +37,20 @@ public abstract class AbstractXmlManagerPluginTest extends AbstractManagerTest {
     private static UIManager ui = new UIManager();
 
     @BeforeClass
-    public static void setUpVirtualProject() {
+    public static void setUpVirtualProject() throws Exception {
         net.bioclipse.core.Activator.getVirtualProject();
+        copyResourceIntoVirtual(
+            "/testFiles/valid.xml",
+            "validRNG.xml"
+        );
+        copyResourceIntoVirtual(
+            "/testFiles/invalid.xml",
+            "invalidRNG.xml"
+        );
+        copyResourceIntoVirtual(
+            "/testFiles/datatype-sample.rng",
+            "schema.rng"
+        );
     }
 
     @Override
@@ -68,8 +91,36 @@ public abstract class AbstractXmlManagerPluginTest extends AbstractManagerTest {
         Assert.fail("Not implemented yet.");
     }
 
-    @Test public void testValidateAgainstRelaxNG() {
-        Assert.fail("Not implemented yet.");
+    private static IFile copyResourceIntoVirtual(
+        String resource, String virtualName) throws Exception {
+        URL url = AbstractXmlManagerPluginTest.class.
+            getResource(resource).toURI().toURL();
+        IProject project = net.bioclipse.core.Activator.getVirtualProject();
+        IFile file = project.getFile(virtualName);
+        if (!file.exists()) {
+            file.create(url.openStream(), IResource.NONE, null);
+        } else {
+            file.setContents(
+                url.openStream(), true, false, new NullProgressMonitor()
+            );
+        }
+        return file;
+    }
+    
+    @Test public void testValidateAgainstRelaxNG() throws Exception {
+        List<XMLError> errors = managerNamespace.validateAgainstRelaxNG(
+            "/Virtual/validRNG.xml",
+            "/Virtual/schema.rng"
+        );
+        Assert.assertEquals(0, errors.size());
+    }
+
+    @Test public void testValidateAgainstRelaxNG_Invalid() throws Exception {
+        List<XMLError> errors = managerNamespace.validateAgainstRelaxNG(
+            "/Virtual/invalidRNG.xml",
+            "/Virtual/schema.rng"
+        );
+        Assert.assertNotSame(0, errors.size());
     }
 
     @Test public void testValidateAgainstXMLSchema() {
