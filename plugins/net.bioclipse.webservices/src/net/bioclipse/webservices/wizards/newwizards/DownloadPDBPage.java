518d9d1584ffc2413097d909f85a5b011dd761f3
Ola Spjuth <ola.spjuth@gmail.com>
2009-10-30 16:40:49 +0100

    Implemented a wizard to query for multiple PDBs.

---
+++ plugins/net.bioclipse.webservices/src/net/bioclipse/webservices/wizards/newwizards/DownloadPDBPage.java
@@ -0,0 +1,58 @@
+package net.bioclipse.webservices.wizards.newwizards;
+
+import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+
+/**
+ * A page offering input of PDBIDs
+ * @author ola
+ *
+ */
+public class DownloadPDBPage extends WizardPage {
+
+    private Text txtPDBid;
+
+    protected DownloadPDBPage(String pageName) {
+        super( pageName );
+    }
+
+    public void createControl( Composite parent ) {
+        
+        setTitle( "Download PDB files" );
+        setDescription( "Please enter one or more PDB ID to download files." );
+
+        Composite composite = new Composite(parent, SWT.NONE);
+        GridLayout gl = new GridLayout(2,false);
+        composite.setLayout(gl);    
+
+        Label lblPDBID = new Label (composite, SWT.NONE);
+        lblPDBID.setText("Enter PDB id's separated by comma: ");
+        GridData gd = new GridData(SWT.LEFT,SWT.CENTER, false, false);
+        gd.horizontalAlignment = GridData.BEGINNING;
+        lblPDBID.setLayoutData(gd);
+
+        txtPDBid = new Text(composite, SWT.BORDER);
+        GridData gd2 = new GridData(SWT.LEFT,SWT.CENTER, true, false);
+//        gd2.horizontalAlignment = GridData.BEGINNING;
+//        gd2.grabExcessHorizontalSpace=true;
+        gd2.widthHint=300;
+        txtPDBid.setLayoutData(gd2);
+        txtPDBid.addModifyListener( new ModifyListener() {
+            public void modifyText( ModifyEvent e ) {
+                ((DownloadPDBWizard)getWizard()).setPdbids( txtPDBid.getText());
+                getWizard().getContainer().updateButtons();
+            }
+        });
+        
+        setControl( composite );
+        
+    }
+    
+}
