d66f22bad5f20be60f942dc11b2ca904376ed84f
Arvid Berg <goglepox@users.sourceforge.net>
2010-10-07 12:09:54 +0200

    Removed empty test class

--- plugins/net.bioclipse.ui.tests/src/net/bioclipse/ui/tests/AllUIPluginTestSuite.java
+++ plugins/net.bioclipse.ui.tests/src/net/bioclipse/ui/tests/AllUIPluginTestSuite.java
@@ -1,7 +1,6 @@
 package net.bioclipse.ui.tests;
 
 import net.bioclipse.ui.tests.feature.FeatureTest;
-import net.bioclipse.ui.views.MyViewTestCase;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -10,8 +9,7 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(value=Suite.class)
 @SuiteClasses( value = { ContentTypesPluginTest.class,
-                         FeatureTest.class,
-                         MyViewTestCase.class} )
+                         FeatureTest.class} )
 public class AllUIPluginTestSuite {
 
 }
