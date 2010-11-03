8f80d5720f0dd031472089e184789a5855d311f0
Arvid Berg <goglepox@users.sourceforge.net>
2010-11-03 10:59:00 +0100

    Added missing copyright statement

--- plugins/net.bioclipse.core/src/net/bioclipse/core/IResourcePathTransformer.java
+++ plugins/net.bioclipse.core/src/net/bioclipse/core/IResourcePathTransformer.java
@@ -1,3 +1,14 @@
+/* *****************************************************************************
+ * Copyright (c) 2007-2009 Bioclipse Project
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors: Jonathan Alvarsson
+ *               Arvid Berg <goglepox@users.sf.net>
+ *
+ *******************************************************************************/
 package net.bioclipse.core;
 
 import org.eclipse.core.resources.IFile;
