3f6ed05e9cb17dd721e532dc8bd2f5011d97c74a
jonalv <jonathan.alvarsson@gmail.com>
2010-12-21 15:44:18 +0100

    oops forgot to comit this (don't know what it is)

---
+++ plugins/net.bioclipse.core.api/src/net/bioclipse/core/api/jobs/ICreateJobAdvice.java
@@ -0,0 +1,12 @@
+package net.bioclipse.core.api.jobs;
+
+import org.aopalliance.intercept.MethodInterceptor;
+
+
+/**
+ * @author jonalv
+ *
+ */
+public interface ICreateJobAdvice extends MethodInterceptor {
+
+}
---
+++ plugins/net.bioclipse.core.api/src/net/bioclipse/core/api/managers/IJavaScriptManagerDispatcherAdvisor.java
@@ -0,0 +1,8 @@
+package net.bioclipse.core.api.managers;
+
+import org.springframework.aop.Advisor;
+
+
+public interface IJavaScriptManagerDispatcherAdvisor extends Advisor {
+
+}
