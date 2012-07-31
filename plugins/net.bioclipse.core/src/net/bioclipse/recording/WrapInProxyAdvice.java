72b7a5f526d987b7f788d4f944cca2fb352d7d0a
jonalv <jonathan.alvarsson@gmail.com>
2012-07-31 15:00:10 +0200

    Make reference local. Fix attempt for bug 3240.
    Attempt at fixing bug 3240. In any way it makes more sense to have this
    variable local as is now the case, regardless of whether this actually fixes
    the bug, or not.

--- plugins/net.bioclipse.core/src/net/bioclipse/recording/WrapInProxyAdvice.java
+++ plugins/net.bioclipse.core/src/net/bioclipse/recording/WrapInProxyAdvice.java
@@ -8,7 +8,6 @@
  *******************************************************************************/
 package net.bioclipse.recording;
 
-import net.bioclipse.recording.Activator;
 import net.bioclipse.core.domain.IBioObject;
 
 import org.aopalliance.intercept.MethodInvocation;
@@ -16,13 +15,12 @@ import org.springframework.aop.framework.ProxyFactory;
 
 public class WrapInProxyAdvice implements IWrapInProxyAdvice {
 
-    private ProxyFactory pf;
     
     public Object invoke(MethodInvocation invocation) throws Throwable {
         Object returnValue = invocation.proceed();
         if(  returnValue instanceof IBioObject && 
            !(returnValue instanceof net.sf.cglib.proxy.Factory) ) {
-            pf = new ProxyFactory();
+            ProxyFactory pf = new ProxyFactory();
             pf.addAdvice(this);
 //            pf.addAdvice(Activator.getDefault().getRecordingAdvice());
             pf.setTarget(returnValue);
