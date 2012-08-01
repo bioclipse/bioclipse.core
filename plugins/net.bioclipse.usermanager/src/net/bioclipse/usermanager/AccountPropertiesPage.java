406393808f937d4d14a38d598a39a5daaec7991d
Klas Jönsson <klas.joensson@gmail.com>
2012-08-01 08:50:50 +0200

    Removed some unnecessary print-statements

--- plugins/net.bioclipse.usermanager/src/net/bioclipse/usermanager/AccountPropertiesPage.java
+++ plugins/net.bioclipse.usermanager/src/net/bioclipse/usermanager/AccountPropertiesPage.java
@@ -249,7 +249,6 @@ public class AccountPropertiesPage {
 	 * respective text-field. 
 	 */
 	public void createAccount() {
-		// TODO remove the comment print-statements
 	    int i = 0;
         IUserManager usermanager = Activator.getDefault().getUserManager();
         HashMap<String, String> properties = new HashMap<String, String>();
@@ -262,20 +261,14 @@ public class AccountPropertiesPage {
 	            accountId = accountType.getName() + "_" + i;
 	        }
 	    } 
-//		System.out.println("Account type: " + accountType.getName());
-//		System.out.println("Account id: "+ accountId);
-//		System.out.println("Account properties:");
 		
 		for (i = 0; i<accountLabels.length;i++) {
-			// This if-statement make sure that repeated fields (e.g. 
-			// "Repeat password") don't end up as a property of the new account.
+			/* This if-statement make sure that repeated fields (e.g. "Repeat 
+			 * password") don't end up as a property of the new account.*/
 			if (!(accountLabels[i].getText().startsWith("Repeat ")) ) {
 				properties.put(accountLabels[i].getText().substring(0, 
 				accountLabels[i].getText().length()-1), 
 				accountTxt[i].getText());		
-//				System.out.println(accountLabels[i].getText().substring(0, 
-//						accountLabels[i].getText().length()-1) +"\t" + 
-//						accountTxt[i].getText());
 			} 
 		}
 		usermanager.createAccount(accountId, properties, accountType);
