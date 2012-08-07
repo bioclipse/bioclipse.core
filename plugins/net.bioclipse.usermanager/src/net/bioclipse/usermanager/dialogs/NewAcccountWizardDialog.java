359a568ea9f5eeef9f25f1d8264b8799d1822ed0
Klas Jönsson <klas.joensson@gmail.com>
2012-08-07 17:49:20 +0200

    creates a dialog for the new account wizard if needed

---
+++ plugins/net.bioclipse.usermanager/src/net/bioclipse/usermanager/dialogs/NewAcccountWizardDialog.java
@@ -0,0 +1,16 @@
+package net.bioclipse.usermanager.dialogs;
+
+import org.eclipse.jface.wizard.IWizard;
+import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.swt.widgets.Shell;
+
+
+public class NewAcccountWizardDialog extends WizardDialog {
+
+    public NewAcccountWizardDialog(Shell parentShell, IWizard newWizard) {
+
+        super( parentShell, newWizard );
+        // TODO Auto-generated constructor stub
+    }
+
+}
