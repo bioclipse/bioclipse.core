d25b9fef6f94bff7e7f623875c38d39f9f645d1a
Klas Jönsson <klas.joensson@gmail.com>
2012-08-13 17:23:48 +0200

    Testing to change the tooltip

--- plugins/net.bioclipse.usermanager/src/net/bioclipse/usermanager/handlers/LogoutHandler.java
+++ plugins/net.bioclipse.usermanager/src/net/bioclipse/usermanager/handlers/LogoutHandler.java
@@ -1,5 +1,7 @@
 package net.bioclipse.usermanager.handlers;
 
+import java.util.Map;
+
 import net.bioclipse.usermanager.Activator;
 
 import org.eclipse.core.commands.AbstractHandler;
@@ -8,11 +10,13 @@ import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.HandlerEvent;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.commands.ICommandService;
+import org.eclipse.ui.commands.IElementUpdater;
 import org.eclipse.ui.internal.commands.CommandService;
 import org.eclipse.ui.internal.commands.CommandServiceFactory;
+import org.eclipse.ui.menus.UIElement;
 import org.eclipse.ui.services.IEvaluationService;
 
-public class LogoutHandler  extends AbstractHandler {
+public class LogoutHandler  extends AbstractHandler implements IElementUpdater {
 
     public Object execute( ExecutionEvent event ) throws ExecutionException {
 
@@ -26,6 +30,13 @@ public class LogoutHandler  extends AbstractHandler {
         commandService.refreshElements("net.bioclipse.usermanager.commands.login", null);
 
         return null;
+    }
+
+    @Override
+    public void updateElement( UIElement element, Map parameters ) {
+        element.setTooltip( "Hej hopp!" );
+        // TODO Auto-generated method stub
+        // When rebased here's where to update the tooltip... But will it work if I log-in to e.g. OpenTox when I create a new account?
     }
 
 }
