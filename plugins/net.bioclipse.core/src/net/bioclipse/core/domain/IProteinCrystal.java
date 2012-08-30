1d25553707b3912ce4f82c762ee421f60ecc1109
Egon Willighagen <egon.willighagen@gmail.com>
2012-08-30 21:22:30 +0200

    Added core bio objects for crystals: small organic (ICrystal) and protein (IProteinCrystal)

---
+++ plugins/net.bioclipse.core/src/net/bioclipse/core/domain/IProteinCrystal.java
@@ -0,0 +1,15 @@
+/* Copyright (c) 2012  Egon Willighagen <egonw@users.sf.net>
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org—epl-v10.html
+ */
+package net.bioclipse.core.domain;
+
+/**
+ * @author egonw
+ */
+public interface IProteinCrystal extends IBioObject {
+
+}
