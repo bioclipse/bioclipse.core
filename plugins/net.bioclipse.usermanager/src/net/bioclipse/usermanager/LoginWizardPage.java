232311dcee503524ac41b90a6662ad7b79f6df7e
Klas Jönsson <klas.joensson@gmail.com>
2012-10-29 10:02:42 +0100

    Removed the not fully implemented help button (bug 3205)

--- plugins/net.bioclipse.usermanager/src/net/bioclipse/usermanager/LoginWizardPage.java
+++ plugins/net.bioclipse.usermanager/src/net/bioclipse/usermanager/LoginWizardPage.java
@@ -15,17 +15,10 @@ import net.bioclipse.usermanager.dialogs.DialogArea;
 import net.bioclipse.usermanager.dialogs.LoginDialog;
 
 import org.apache.log4j.Logger;
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Status;
-import org.eclipse.core.runtime.SubProgressMonitor;
-import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.handlers.IHandlerService;
 
 /**
  * This is the wizard page for log-in into Bioclipse.
@@ -98,10 +91,5 @@ public class LoginWizardPage extends WizardPage {
 	        super.setVisible( visible );  
 	}
 
-	@Override
-	public void performHelp() {
-	    System.out.println("Perform help in LoginWizardPage");
-	    PlatformUI.getWorkbench().getHelpSystem().displayHelpResource( "./html/AccountHelp.html" );
-	}
 }
 
