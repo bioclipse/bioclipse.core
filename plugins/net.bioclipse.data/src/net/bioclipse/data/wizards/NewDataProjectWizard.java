dabac00d4f2240f8de671d9457c6812b4b4bca2d
Klas Jönsson <klas.joensson@gmail.com>
2012-10-29 10:48:46 +0100

    Fixed the window title

--- plugins/net.bioclipse.data/src/net/bioclipse/data/wizards/NewDataProjectWizard.java
+++ plugins/net.bioclipse.data/src/net/bioclipse/data/wizards/NewDataProjectWizard.java
@@ -231,7 +231,7 @@ public class NewDataProjectWizard extends Wizard implements INewWizard,
     public void init(IWorkbench workbench, IStructuredSelection selection) {
         this.workbench = workbench;
         this.selection = selection;
-        setWindowTitle("New Sample Data Project");
+        setWindowTitle( createPageName() );
 //        setDefaultPageImageDescriptor(TBC);
     }
 
