98aa4863ac57ea99b0d62e7fddc7df4f21d2e9a5
Klas Jönsson <klas.joensson@gmail.com>
2013-02-22 13:50:13 +0100

    Add a help context provider for text editors

---
+++ plugins/net.bioclipse.ui/src/net/bioclipse/ui/editors/TextEditorHelpContextProvider.java
@@ -0,0 +1,32 @@
+package net.bioclipse.ui.editors;
+
+import org.eclipse.help.HelpSystem;
+import org.eclipse.help.IContext;
+import org.eclipse.help.IContextProvider;
+import org.eclipse.ui.editors.text.TextEditor;
+
+
+public class TextEditorHelpContextProvider implements IContextProvider {
+
+    private TextEditor editor;
+    
+    public TextEditorHelpContextProvider(TextEditor editor) {
+        this.editor = editor;
+    }
+    
+    @Override
+    public int getContextChangeMask() {
+        return IContextProvider.NONE;
+    }
+
+    @Override
+    public IContext getContext( Object target ) {
+        return HelpSystem.getContext( "net.bioclipse.ui.accountWizardHelp" );
+    }
+
+    @Override
+    public String getSearchExpression( Object target ) {
+        return null;
+    }
+
+}
