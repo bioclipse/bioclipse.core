4af5acd3ac5dae946288145c5f27d0328dc43c0e
Egon Willighagen <egon.willighagen@gmail.com>
2015-08-22 11:58:40 +0200

    Added a manager to allow me to use jsoup.
    An example:
    
    htmlFilename = "/ECHA/substance.html";
    dossierFolder = "http://apps.echa.europa.eu/registered/data/dossiers/";
    testURL = dossierFolder + "DISS-9d8ad2a1-0d51-13f7-e044-00144f67d249/AGGR-7e14e77e-8a7d-4f8d-ab80-d0b7df6bb939_DISS-9d8ad2a1-0d51-13f7-e044-00144f67d249.html";
    
    htmlFile = bioclipse.downloadAsFile(testURL, "text/html", htmlFilename);
    
    doc = jsoup.parse(htmlFilename);
    
    id = jsoup.select(doc, "#body .structuralFormula img").attr("src").split("_")[0];
    name = jsoup.select(doc, "#body .name .value").text()
    ecNumber = jsoup.select(doc, "#body .ecNumber .value").text()
    casNumber = jsoup.select(doc, "#body .casNumber .value").text()
    molecularFormula = jsoup.select(doc, "#body .molecularFormula .value").first().text()
    iupacName = jsoup.select(doc, "#body .iupacName .value").text()
    smiles = jsoup.select(doc, "#body .smilesNotation .value span").text()
    inchi = jsoup.select(doc, "#body .inchi .value span").text()

---
+++ plugins/net.bioclipse.jsoup.business/src/net/bioclipse/jsoup/business/business/IJavaScriptJSoupManager.java
@@ -0,0 +1,18 @@
+/*******************************************************************************
+ * Copyright (c) 2015  Egon Willighagen <egon.willighagen@gmail.com>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contact: Bioclipse Project <http://www.bioclipse.net>
+ ******************************************************************************/
+package net.bioclipse.jsoup.business.business;
+
+import net.bioclipse.managers.business.IBioclipseJSManager;
+
+public interface IJavaScriptJSoupManager
+       extends IJSoupManager, IBioclipseJSManager {
+
+}
